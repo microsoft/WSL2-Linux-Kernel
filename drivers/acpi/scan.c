c1493a189c8dcbd276144b958f9c4cb4bd25940c
Thomas Renninger <trenn@suse.de>
2005-12-02 20:39:57 +0000

    [PATCH] ACPI: fix HP nx8220 boot hang regression
    [ACPI] fix HP nx8220 boot hang regression
    
    This patch reverts the acpi_bus_find_driver() return value check
    that came in via the PCI tree via 3fb02738b0fd36f47710a2bf207129efd2f5daa2
    
            [PATCH] acpi bridge hotadd: Allow ACPI .add and .start
    	operations to be done independently
    
    This particular change broke booting of some HP/Compaq laptops unless
    acpi=noirq is used.
    
    http://bugzilla.kernel.org/show_bug.cgi?id=5221
    https://bugzilla.novell.com/show_bug.cgi?id=116763
    
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    Cc: Rajesh Shah <rajesh.shah@intel.com>
    Signed-off-by: Len Brown <len.brown@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -1111,7 +1111,7 @@ acpi_add_single_object(struct acpi_device **child,
 	 *
 	 * TBD: Assumes LDM provides driver hot-plug capability.
 	 */
-	result = acpi_bus_find_driver(device);
+	acpi_bus_find_driver(device);
 
       end:
 	if (!result)
