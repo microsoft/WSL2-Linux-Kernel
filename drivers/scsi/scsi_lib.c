841f70676036b309f7102e2c8024dc68c3946990
Stefan Richter <stefanr@s5r6.in-berlin.de>
2005-12-14 23:34:11 +0100

    [PATCH] SCSI: fix transfer direction in scsi_lib and st
    SCSI: fix transfer direction in scsi_lib and st
    
    scsi_prep_fn and st_init_command could issue WRITE requests with zero
    buffer length. This may lead to kernel panic or oops with some SCSI
    low-level drivers.
    
    Derived from -rc patches from Jens Axboe and James Bottomley.
    
    Patch is reassembled for -stable from patches:
    [SCSI] fix panic when ejecting ieee1394 ipod
    [SCSI] Consolidate REQ_BLOCK_PC handling path (fix ipod panic)
    
    Depends on patch "SCSI: fix transfer direction in sd (kernel panic when
    ejecting iPod)". Also modifies the already correct sr_init_command to
    fully match the corresponding -rc patch.
    
    Signed-off-by: Stefan Richter <stefanr@s5r6.in-berlin.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -1284,18 +1284,7 @@ static int scsi_prep_fn(struct request_queue *q, struct request *req)
 				goto kill;
 			}
 		} else {
-			memcpy(cmd->cmnd, req->cmd, sizeof(cmd->cmnd));
-			cmd->cmd_len = req->cmd_len;
-			if (rq_data_dir(req) == WRITE)
-				cmd->sc_data_direction = DMA_TO_DEVICE;
-			else if (req->data_len)
-				cmd->sc_data_direction = DMA_FROM_DEVICE;
-			else
-				cmd->sc_data_direction = DMA_NONE;
-			
-			cmd->transfersize = req->data_len;
-			cmd->allowed = 3;
-			cmd->timeout_per_command = req->timeout;
+			scsi_setup_blk_pc_cmnd(cmd, 3);
 			cmd->done = scsi_generic_done;
 		}
 	}
