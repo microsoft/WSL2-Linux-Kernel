5fa8ad644faa114c4190484ac50e15236fc7cdd9
Andi Kleen <ak@muc.de>
2006-01-21 13:13:13 -0800

    [PATCH] Mask off GFP flags before swiotlb_alloc_coherent
    Mask off GFP flags before swiotlb_alloc_coherent
    
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Chris Wright <chris@sous-sol.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86_64/kernel/pci-gart.c
+++ arch/x86_64/kernel/pci-gart.c
@@ -244,6 +244,7 @@ dma_alloc_coherent(struct device *dev, size_t size, dma_addr_t *dma_handle,
 					   get_order(size));
 
 				if (swiotlb) {
+					gfp &= ~(GFP_DMA32|GFP_DMA);
 					return
 					swiotlb_alloc_coherent(dev, size,
 							       dma_handle,
