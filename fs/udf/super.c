b9a96aa8fa91f4f3187d141a84f0aeaed2935cbe
Jan Kara <jack@suse.cz>
2006-08-23 18:00:30 +0200

    Fix possible UDF deadlock and memory corruption (CVE-2006-4145)
    UDF code is not really ready to handle extents larger that 1GB. This is
    the easy way to forbid creating those.
    
    Also truncation code did not count with the case when there are no
    extents in the file and we are extending the file.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- fs/udf/super.c
+++ fs/udf/super.c
@@ -1657,7 +1657,7 @@ static int udf_fill_super(struct super_block *sb, void *options, int silent)
 		iput(inode);
 		goto error_out;
 	}
-	sb->s_maxbytes = MAX_LFS_FILESIZE;
+	sb->s_maxbytes = 1<<30;
 	return 0;
 
 error_out:
