da56aea0bddc155f786594720ab91e36421cc51e
Chen-Li Tien <cltien@gmail.com>
2006-09-06 19:34:53 +0200

    [PKTGEN]: Fix oops when used with balance-tlb bonding
    Signed-off-by: Chen-Li Tien <cltien@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- net/core/pktgen.c
+++ net/core/pktgen.c
@@ -1902,6 +1902,8 @@ static struct sk_buff *fill_packet_ipv4(struct net_device *odev,
 	skb->mac.raw = ((u8 *)iph) - 14;
 	skb->dev = odev;
 	skb->pkt_type = PACKET_HOST;
+	skb->nh.iph = iph;
+	skb->h.uh = udph;
 
 	if (pkt_dev->nfrags <= 0) 
                 pgh = (struct pktgen_hdr *)skb_put(skb, datalen);
