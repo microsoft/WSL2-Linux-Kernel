a030daed9949daa6746072ee2752217adc424252
Andrew Morton <akpm@osdl.org>
2006-12-06 20:31:30 -0800

    [PATCH] add bottom_half.h
    With CONFIG_SMP=n:
    
    drivers/input/ff-memless.c:384: warning: implicit declaration of function 'local_bh_disable'
    drivers/input/ff-memless.c:393: warning: implicit declaration of function 'local_bh_enable'
    
    Really linux/spinlock.h should include linux/interrupt.h.  But interrupt.h
    includes sched.h which will need spinlock.h.
    
    So the patch breaks the _bh declarations out into a separate header and
    includes it in bothj interrupt.h and spinlock.h.
    
    Cc: "Randy.Dunlap" <rdunlap@xenotime.net>
    Cc: Andi Kleen <ak@suse.de>
    Cc: <stable@kernel.org>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@osdl.org>
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>

---
+++ include/linux/bottom_half.h
@@ -0,0 +1,5 @@
+extern void local_bh_disable(void);
+extern void __local_bh_enable(void);
+extern void _local_bh_enable(void);
+extern void local_bh_enable(void);
+extern void local_bh_enable_ip(unsigned long ip);
