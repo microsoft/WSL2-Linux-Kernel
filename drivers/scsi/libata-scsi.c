1bd73c60d5d9c9d80ebdad644b79f8a96e5d825f
Jeff Garzik <jeff@garzik.org>
2007-02-03 02:32:59 +0100

    [libata] use kmap_atomic(KM_IRQ0) in SCSI simulator
    We are inside spin_lock_irqsave().  quoth akpm's debug facility:
    
     [  231.948000] SCSI device sda: 195371568 512-byte hdwr sectors (100030 MB)
     [  232.232000] ata1.00: configured for UDMA/33
     [  232.404000] WARNING (1) at arch/i386/mm/highmem.c:47 kmap_atomic()
     [  232.404000]  [<c01162e6>] kmap_atomic+0xa9/0x1ab
     [  232.404000]  [<c0242c81>] ata_scsi_rbuf_get+0x1c/0x30
     [  232.404000]  [<c0242caf>] ata_scsi_rbuf_fill+0x1a/0x87
     [  232.404000]  [<c0243ab2>] ata_scsiop_mode_sense+0x0/0x309
     [  232.404000]  [<c01729d5>] end_bio_bh_io_sync+0x0/0x37
     [  232.404000]  [<c02311c6>] scsi_done+0x0/0x16
     [  232.404000]  [<c02311c6>] scsi_done+0x0/0x16
     [  232.404000]  [<c0242dcc>] ata_scsi_simulate+0xb0/0x13f
    [...]
    
    Signed-off-by: Jeff Garzik <jeff@garzik.org>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- drivers/scsi/libata-scsi.c
+++ drivers/scsi/libata-scsi.c
@@ -1372,7 +1372,7 @@ static unsigned int ata_scsi_rbuf_get(struct scsi_cmnd *cmd, u8 **buf_out)
 		struct scatterlist *sg;
 
 		sg = (struct scatterlist *) cmd->request_buffer;
-		buf = kmap_atomic(sg->page, KM_USER0) + sg->offset;
+		buf = kmap_atomic(sg->page, KM_IRQ0) + sg->offset;
 		buflen = sg->length;
 	} else {
 		buf = cmd->request_buffer;
@@ -1400,7 +1400,7 @@ static inline void ata_scsi_rbuf_put(struct scsi_cmnd *cmd, u8 *buf)
 		struct scatterlist *sg;
 
 		sg = (struct scatterlist *) cmd->request_buffer;
-		kunmap_atomic(buf - sg->offset, KM_USER0);
+		kunmap_atomic(buf - sg->offset, KM_IRQ0);
 	}
 }
 
