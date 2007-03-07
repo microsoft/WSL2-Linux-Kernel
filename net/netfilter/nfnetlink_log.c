08bfad45bd7e07dad209f593499fea2d05c1f75c
Patrick McHardy <kaber@trash.net>
2007-03-07 22:34:33 +0100

    nfnetlink_log: fix use after free
    [NETFILTER]: nfnetlink_log: fix use after free
    
    Paranoia: instance_put() might have freed the inst pointer when we
    spin_unlock_bh().
    
    Signed-off-by: Michal Miroslaw <mirq-linux@rere.qmqm.pl>
    Signed-off-by: Patrick McHardy <kaber@trash.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- net/netfilter/nfnetlink_log.c
+++ net/netfilter/nfnetlink_log.c
@@ -397,8 +397,8 @@ static void nfulnl_timer(unsigned long data)
 	if (timer_pending(&inst->timer))	/* is it always true or false here? */
 		del_timer(&inst->timer);
 	__nfulnl_send(inst);
-	instance_put(inst);
 	spin_unlock_bh(&inst->lock);
+	instance_put(inst);
 }
 
 /* This is an inline function, we don't really care about a long
