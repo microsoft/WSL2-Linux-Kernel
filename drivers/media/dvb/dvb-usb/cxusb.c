8cf5c27708b395705f9878e3b444dd9378e14f60
Jin-Bong lee <jinbong.lee@samsung.com>
2007-03-08 08:41:19 +0100

    DVB: cxusb: fix firmware patch for big endian systems
    Without this patch, the device will not be detected after firmware download
    on big endian systems.
    
    Signed-off-by: Jin-Bong lee <jinbong.lee@samsung.com>
    Signed-off-by: Michael Krufky <mkrufky@linuxtv.org>
    Signed-off-by: Adrian Bunk <bunk@stusta.de>

--- drivers/media/dvb/dvb-usb/cxusb.c
+++ drivers/media/dvb/dvb-usb/cxusb.c
@@ -420,8 +420,8 @@ static int bluebird_patch_dvico_firmware_download(struct usb_device *udev, const
 	    fw->data[BLUEBIRD_01_ID_OFFSET + 1] == USB_VID_DVICO >> 8) {
 
 		/* FIXME: are we allowed to change the fw-data ? */
-		fw->data[BLUEBIRD_01_ID_OFFSET + 2] = udev->descriptor.idProduct + 1;
-		fw->data[BLUEBIRD_01_ID_OFFSET + 3] = udev->descriptor.idProduct >> 8;
+		fw->data[BLUEBIRD_01_ID_OFFSET + 2] = le16_to_cpu(udev->descriptor.idProduct) + 1;
+		fw->data[BLUEBIRD_01_ID_OFFSET + 3] = le16_to_cpu(udev->descriptor.idProduct) >> 8;
 
 		return usb_cypress_load_firmware(udev,fw,CYPRESS_FX2);
 	}
