b44da25c30867fa71f2cec3334ecfa1e9412dab4
Andi Kleen <ak@suse.de>
2007-08-10 22:31:07 +0200

    i386: Use global flag to disable broken local apic timer on AMD CPUs.
    commit d3f7eae182b04997be19343a23f7009170f4f7a5 upstream
    
    The Averatec 2370 and some other Turion laptop BIOS seems to program the
    ENABLE_C1E MSR inconsistently between cores. This confuses the lapic
    use heuristics because when C1E is enabled anywhere it seems to affect
    the complete chip.
    
    Use a global flag instead of a per cpu flag to handle this.
    If any CPU has C1E enabled disabled lapic use.
    
    Thanks to Cal Peake for debugging.
    
    Cc: tglx@linutronix.de
    Cc: Chuck Ebbert <cebbert@redhat.com>
    Signed-off-by: Andi Kleen <ak@suse.de>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/asm-i386/cpufeature.h
+++ include/asm-i386/cpufeature.h
@@ -79,7 +79,7 @@
 #define X86_FEATURE_ARCH_PERFMON (3*32+11) /* Intel Architectural PerfMon */
 #define X86_FEATURE_PEBS	(3*32+12)  /* Precise-Event Based Sampling */
 #define X86_FEATURE_BTS		(3*32+13)  /* Branch Trace Store */
-#define X86_FEATURE_LAPIC_TIMER_BROKEN (3*32+ 14) /* lapic timer broken in C1 */
+/* 14 free */
 #define X86_FEATURE_SYNC_RDTSC	(3*32+15)  /* RDTSC synchronizes the CPU */
 
 /* Intel-defined CPU features, CPUID level 0x00000001 (ecx), word 4 */
