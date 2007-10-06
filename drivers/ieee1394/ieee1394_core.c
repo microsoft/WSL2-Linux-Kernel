3f5514bf3e051044e91d17aab46ee2191a4cace2
Adrian Bunk <bunk@kernel.org>
2007-10-06 22:05:29 +0200

    unexport csr1212_release_keyval
    A static function mustn't be exported.
    
    Signed-off-by: Adrian Bunk <bunk@kernel.org>

--- drivers/ieee1394/ieee1394_core.c
+++ drivers/ieee1394/ieee1394_core.c
@@ -1306,7 +1306,6 @@ EXPORT_SYMBOL(hpsb_iso_recv_flush);
 EXPORT_SYMBOL(csr1212_new_directory);
 EXPORT_SYMBOL(csr1212_attach_keyval_to_directory);
 EXPORT_SYMBOL(csr1212_detach_keyval_from_directory);
-EXPORT_SYMBOL(csr1212_release_keyval);
 EXPORT_SYMBOL(csr1212_read);
 EXPORT_SYMBOL(csr1212_parse_keyval);
 EXPORT_SYMBOL(_csr1212_read_keyval);
