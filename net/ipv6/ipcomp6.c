6c7ad737a3134d1b778bf60e7e4b323d662dfadd
Herbert Xu <herbert@gondor.apana.org.au>
2007-11-13 02:48:28 -0800

    Fix crypto_alloc_comp() error checking.
    [IPSEC]: Fix crypto_alloc_comp error checking
    
    [ Upstream commit: 4999f3621f4da622e77931b3d33ada6c7083c705 ]
    
    The function crypto_alloc_comp returns an errno instead of NULL
    to indicate error.  So it needs to be tested with IS_ERR.
    
    This is based on a patch by Vicenç Beltran Querol.
    
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- net/ipv6/ipcomp6.c
+++ net/ipv6/ipcomp6.c
@@ -37,6 +37,7 @@
 #include <asm/scatterlist.h>
 #include <asm/semaphore.h>
 #include <linux/crypto.h>
+#include <linux/err.h>
 #include <linux/pfkeyv2.h>
 #include <linux/random.h>
 #include <linux/percpu.h>
@@ -366,7 +367,7 @@ static struct crypto_comp **ipcomp6_alloc_tfms(const char *alg_name)
 	for_each_possible_cpu(cpu) {
 		struct crypto_comp *tfm = crypto_alloc_comp(alg_name, 0,
 							    CRYPTO_ALG_ASYNC);
-		if (!tfm)
+		if (IS_ERR(tfm))
 			goto error;
 		*per_cpu_ptr(tfms, cpu) = tfm;
 	}
