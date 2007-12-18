3adcc285eaca8de3e576a122c1824c293483af8b
Greg Kroah-Hartman <gregkh@suse.de>
2007-12-18 13:48:22 -0800

    Revert "PNP: increase the maximum number of resources"
    This reverts commit fc175adc1c935ea8679d76a78d7a58df34af16eb.
    
    There have been reports that it causes problems:
    	http://bugzilla.kernel.org/show_bug.cgi?id=9514
    people are still debating for 2.6.24 if it should be reverted or not,
    but as it causes a known problem, we will revert this for now.
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pnp/pnpacpi/rsparser.c
+++ drivers/pnp/pnpacpi/rsparser.c
@@ -82,11 +82,9 @@ static void pnpacpi_parse_allocated_irqresource(struct pnp_resource_table *res,
 	while (!(res->irq_resource[i].flags & IORESOURCE_UNSET) &&
 	       i < PNP_MAX_IRQ)
 		i++;
-	if (i >= PNP_MAX_IRQ) {
-		printk(KERN_ERR "pnpacpi: exceeded the max number of IRQ "
-				"resources: %d \n", PNP_MAX_IRQ);
+	if (i >= PNP_MAX_IRQ)
 		return;
-	}
+
 	res->irq_resource[i].flags = IORESOURCE_IRQ;	// Also clears _UNSET flag
 	res->irq_resource[i].flags |= irq_flags(triggering, polarity);
 	irq = acpi_register_gsi(gsi, triggering, polarity);
@@ -165,9 +163,6 @@ static void pnpacpi_parse_allocated_dmaresource(struct pnp_resource_table *res,
 		}
 		res->dma_resource[i].start = dma;
 		res->dma_resource[i].end = dma;
-	} else {
-		printk(KERN_ERR "pnpacpi: exceeded the max number of DMA "
-				"resources: %d \n", PNP_MAX_DMA);
 	}
 }
 
@@ -189,9 +184,6 @@ static void pnpacpi_parse_allocated_ioresource(struct pnp_resource_table *res,
 		}
 		res->port_resource[i].start = io;
 		res->port_resource[i].end = io + len - 1;
-	} else {
-		printk(KERN_ERR "pnpacpi: exceeded the max number of IO "
-				"resources: %d \n", PNP_MAX_PORT);
 	}
 }
 
@@ -215,9 +207,6 @@ static void pnpacpi_parse_allocated_memresource(struct pnp_resource_table *res,
 
 		res->mem_resource[i].start = mem;
 		res->mem_resource[i].end = mem + len - 1;
-	} else {
-		printk(KERN_ERR "pnpacpi: exceeded the max number of mem "
-				"resources: %d\n", PNP_MAX_MEM);
 	}
 }
 
