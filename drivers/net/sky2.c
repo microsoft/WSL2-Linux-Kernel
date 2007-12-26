e62eb48fc190c23187e6f00b211439ddec940e2e
Stephen Hemminger <shemminger@linux-foundation.org>
2007-12-26 09:59:26 -0800

    sky2: disable rx checksum on Yukon XL
    Backport of 8b31cfbcd1b54362ef06c85beb40e65a349169a2
    
    The Marvell Yukon XL chipset appears to have a hardware glitch
    where it will repeat the checksum of the last packet. Of course, this is
    timing sensitive and only happens sometimes...
    
    More info: http://bugzilla.kernel.org/show_bug.cgi?id=9381
    
    As a workaround just disable hardware checksumming by default on
    this chip version. The earlier workaround for PCIX, dual port
    was also on Yukon XL so don't need to disable checksumming there.
    
    Signed-off-by: Stephen Hemminger <shemminger@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/sky2.c
+++ drivers/net/sky2.c
@@ -1307,15 +1307,11 @@ static int sky2_up(struct net_device *dev)
 	 */
 	if (otherdev && netif_running(otherdev) &&
  	    (cap = pci_find_capability(hw->pdev, PCI_CAP_ID_PCIX))) {
- 		struct sky2_port *osky2 = netdev_priv(otherdev);
  		u16 cmd;
 
  		cmd = sky2_pci_read16(hw, cap + PCI_X_CMD);
  		cmd &= ~PCI_X_CMD_MAX_SPLIT;
  		sky2_pci_write16(hw, cap + PCI_X_CMD, cmd);
-
- 		sky2->rx_csum = 0;
- 		osky2->rx_csum = 0;
  	}
 
 	if (netif_msg_ifup(sky2))
@@ -4017,7 +4013,7 @@ static __devinit struct net_device *sky2_init_netdev(struct sky2_hw *hw,
 	sky2->duplex = -1;
 	sky2->speed = -1;
 	sky2->advertising = sky2_supported_modes(hw);
-	sky2->rx_csum = 1;
+	sky2->rx_csum = (hw->chip_id != CHIP_ID_YUKON_XL);
 	sky2->wol = wol;
 
 	spin_lock_init(&sky2->phy_lock);
