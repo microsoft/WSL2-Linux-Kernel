f2336eeb199d3ed113e6d01c8020ea8919dd85c9
Dan Williams <dan.j.williams@intel.com>
2008-03-04 19:40:09 +0000

    ioat: fix 'ack' handling, driver must ensure that 'ack' is zero
    commit: 6497dcffe07b7c3d863f9899280c4f6eae999161
    
    Initialize 'ack' to zero in case the descriptor has been recycled.
    
    Prevents "kernel BUG at crypto/async_tx/async_xor.c:185!"
    
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    Acked-by: Shannon Nelson <shannon.nelson@intel.com>
    [chrisw@sous-sol.org: backport to 2.6.24.3]
    Signed-off-by: Chris Wright <chrisw@sous-sol.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/dma/ioat_dma.c
+++ drivers/dma/ioat_dma.c
@@ -726,6 +726,7 @@ static struct dma_async_tx_descriptor *ioat1_dma_prep_memcpy(
 
 	if (new) {
 		new->len = len;
+		new->async_tx.ack = 0;
 		return &new->async_tx;
 	} else
 		return NULL;
@@ -749,6 +750,7 @@ static struct dma_async_tx_descriptor *ioat2_dma_prep_memcpy(
 
 	if (new) {
 		new->len = len;
+		new->async_tx.ack = 0;
 		return &new->async_tx;
 	} else
 		return NULL;
