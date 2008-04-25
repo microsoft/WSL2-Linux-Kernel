050aa73d317bb6f8182c1ee8d4850c3aee873d7a
Sreenivasa Honnur <Sreenivasa.Honnur@neterion.com>
2008-04-25 13:22:41 -0400

    S2io: Version update for memory leak fix during free_tx_buffers
    commit 10371b5e6ba22173425877ea6a7040619b005fa1 upstream
    
    - Updated version number.
    
    Signed-off-by: Santosh Rastapur <santosh.rastapur@neterion.com>
    Signed-off-by: Ramkrishna Vepa <ram.vepa@neterion.com>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/s2io.c
+++ drivers/net/s2io.c
@@ -84,7 +84,7 @@
 #include "s2io.h"
 #include "s2io-regs.h"
 
-#define DRV_VERSION "2.0.26.20"
+#define DRV_VERSION "2.0.26.22"
 
 /* S2io Driver name & version. */
 static char s2io_driver_name[] = "Neterion";
