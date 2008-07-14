873496a3485950402ee0436c9d17eeb789157b10
David S. Miller <davem@davemloft.net>
2008-07-14 21:09:23 +0300

    sit: Add missing kfree_skb() on pskb_may_pull() failure. (CVE-2008-2136)
    Noticed by Paul Marks <paul@pmarks.net>.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Adrian Bunk <bunk@kernel.org>

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -398,9 +398,9 @@ static int ipip6_rcv(struct sk_buff *skb)
 	}
 
 	icmp_send(skb, ICMP_DEST_UNREACH, ICMP_PROT_UNREACH, 0);
-	kfree_skb(skb);
 	read_unlock(&ipip6_lock);
 out:
+	kfree_skb(skb);
 	return 0;
 }
 
