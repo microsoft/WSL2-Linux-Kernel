4b09fd1b359a42f910881feca140ae0bf3efb28e
Jeff Dike <jdike@addtoit.com>
2008-08-05 16:14:09 -0400

    uml: memcpy export needs to follow host declaration
    commit 8bfd04b974689f700bbd053ad6e66b0a95fb80c9 upstream
    
    x86_64 defines either memcpy or __memcpy depending on the gcc version, and
    it looks like UML needs to follow that in its exporting.
    
    Cc: Gabriel C <nix.or.die@googlemail.com>
    Signed-off-by: Jeff Dike <jdike@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/um/sys-x86_64/ksyms.c
+++ arch/um/sys-x86_64/ksyms.c
@@ -13,5 +13,9 @@ EXPORT_SYMBOL(__down_failed_trylock);
 EXPORT_SYMBOL(__up_wakeup);
 
 /*XXX: we need them because they would be exported by x86_64 */
+#if (__GNUC__ == 4 && __GNUC_MINOR__ >= 3) || __GNUC__ > 4
+EXPORT_SYMBOL(memcpy);
+#else
 EXPORT_SYMBOL(__memcpy);
+#endif
 EXPORT_SYMBOL(csum_partial);
