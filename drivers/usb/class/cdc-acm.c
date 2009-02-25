e8c0f6bbd71a4612ac604879593be7b8d225d28c
Dmitriy Taychenachev <dimichxp@gmail.com>
2009-02-25 12:36:51 +0800

    USB: cdc-acm: add usb id for motomagx phones
    commit 155df65ae11dfc322214c6f887185929c809df1b upstream.
    
    The Motorola MOTOMAGX phones (Z6, E8, Zn5 so far) are providing
    combined ACM/BLAN USB configuration. Since it has Vendor Specific
    class, the corresponding drivers (cdc-acm, zaurus) can't find it just
    by interface info. This patch adds usb id so the cdc-acm driver can
    properly handle this combined device.
    
    Signed-off-by: Dmitriy Taychenachev <dimichxp@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/class/cdc-acm.c
+++ drivers/usb/class/cdc-acm.c
@@ -1370,6 +1370,8 @@ static struct usb_device_id acm_ids[] = {
 	{ USB_DEVICE(0x0572, 0x1321), /* Conexant USB MODEM CX93010 */
 	.driver_info = NO_UNION_NORMAL, /* has no union descriptor */
 	},
+	{ USB_DEVICE(0x22b8, 0x6425), /* Motorola MOTOMAGX phones */
+	},
 
 	/* control interfaces with various AT-command sets */
 	{ USB_INTERFACE_INFO(USB_CLASS_COMM, USB_CDC_SUBCLASS_ACM,
