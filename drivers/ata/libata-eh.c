50d4eeb0d1d7eed0f13726be4dbb4c4b637e3a9b
Tejun Heo <tj@kernel.org>
2009-03-04 15:59:30 +0900

    libata: make sure port is thawed when skipping resets
    commit d6515e6ff4ad3db4bd5ef2dd4e1026a7aca2482e upstream.
    
    When SCR access is available and the link is offline, softreset is
    skipped as it only wastes time and some controllers don't respond very
    well.  However, the skip path forgot to thaw the port, which not only
    blocks further event notification from the port but also causes
    repeated EH invocations on the same event on drivers which rely on
    ->thaw() to clear events if the IRQ is shared with another device or
    port.
    
    This problem has always been there but is uncovered by recent sata_nv
    nf2/3 change which dropped hardreset support while maintaining SCR
    access.  nf2/3 doesn't clear hotplug event mask from the interrupt
    handler but relies on ->thaw() to clear them.  When the hardreset was
    there, the reset action was never skipped and the port was always
    thawed but, with the hardreset gone, ->prereset() determines that
    there's no need for softreset and both ->softreset() and ->thaw() are
    skipped.  This leads to stuck hotplug event in the IRQ status register
    triggering hotplug event whenever IRQ is delieverd on the same IRQ.
    As the controller shares the same IRQ for both ports, this happens on
    every IO if one port is occpupied and the other isn't.
    
    This patch fixes the problem by making sure that the port is thawed on
    reset-skip path.
    
    bko#11615 reports this problem.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Cc: Robert Hancock <hancockrwd@gmail.com>
    Reported-by: Dan Andresan <danyer@gmail.com>
    Reported-by: Arne Woerner <arne_woerner@yahoo.com>
    Reported-by: Stefan Lippers-Hollmann <s.L-H@gmx.de>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/ata/libata-eh.c
+++ drivers/ata/libata-eh.c
@@ -2366,11 +2366,14 @@ int ata_eh_reset(struct ata_link *link, int classify,
 		}
 
 		/* prereset() might have cleared ATA_EH_RESET.  If so,
-		 * bang classes and return.
+		 * bang classes, thaw and return.
 		 */
 		if (reset && !(ehc->i.action & ATA_EH_RESET)) {
 			ata_link_for_each_dev(dev, link)
 				classes[dev->devno] = ATA_DEV_NONE;
+			if ((ap->pflags & ATA_PFLAG_FROZEN) &&
+			    ata_is_host_link(link))
+				ata_eh_thaw_port(ap);
 			rc = 0;
 			goto out;
 		}
