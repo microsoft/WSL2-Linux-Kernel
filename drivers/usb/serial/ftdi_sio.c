c9641d3c6e3fd911de1c6150fb11da80a10b4fc0
Axel Wachtler <axel.wachtler@atmel.com>
2009-03-05 14:09:22 -0800

    USB: serial: add FTDI USB/Serial converter devices
    commit 7f82b6dd7015aabca2fd55fb690248f742cd67f3 upstream.
    
    Add the following devices to the USB FTDI SIO device table:
    
     Bus 001 Device 009: ID 03eb:2109 Atmel Corp.
     http://www.atmel.com/dyn/products/tools_card.asp?tool_id=4187
    
     Bus 001 Device 008: ID 1cf1:0001
     http://www.dresden-elektronik.de/shop/prod75.html
    
     Bus 001 Device 007: ID 1c1f:0004
     http://www.dresden-elektronik.de/shop/prod64.html
    
    Signed-off-by: Axel Wachtler <axel.wachtler@atmel.com>
    Signed-off-by: Robert Richter <robert.richter@amd.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -657,6 +657,9 @@ static struct usb_device_id id_table_combined [] = {
 	{ USB_DEVICE(PAPOUCH_VID, PAPOUCH_QUIDO4x4_PID) },
 	{ USB_DEVICE(FTDI_VID, FTDI_DOMINTELL_DGQG_PID) },
 	{ USB_DEVICE(FTDI_VID, FTDI_DOMINTELL_DUSB_PID) },
+	{ USB_DEVICE(ATMEL_VID, STK541_PID) },
+	{ USB_DEVICE(DE_VID, STB_PID) },
+	{ USB_DEVICE(DE_VID, WHT_PID) },
 	{ },					/* Optional parameter entry */
 	{ }					/* Terminating entry */
 };
