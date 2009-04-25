0f6e7a29d6a4865951b8ae598fb5594a6eb54b54
Hugh Dickins <hugh@veritas.com>
2009-04-25 17:52:56 +0100

    fs core fixes
    Please add the following 4 commits to 2.6.27-stable and 2.6.28-stable.
    However, there has been a lot of change here between 2.6.28 and 2.6.29:
    in particular, fs/exec.c's unsafe_exec() grew into the more complicated
    check_unsafe_exec().  So applying the original patches gives too many
    rejects: at the bottom is the diffstat and the combined patch required.
    
    1
    Commit: 53e9309e01277ec99c38e84e0ca16921287cf470
    Author: Hugh Dickins <hugh@veritas.com>
    Date: Sat, 28 Mar 2009 23:16:03 +0000 (+0000)
    Subject: compat_do_execve should unshare_files
    
    2
    Commit: e426b64c412aaa3e9eb3e4b261dc5be0d5a83e78
    Author: Hugh Dickins <hugh@veritas.com>
    Date: Sat, 28 Mar 2009 23:20:19 +0000 (+0000)
    Subject: fix setuid sometimes doesn't
    
    3
    Commit: 7c2c7d993044cddc5010f6f429b100c63bc7dffb
    Author: Hugh Dickins <hugh@veritas.com>
    Date: Sat, 28 Mar 2009 23:21:27 +0000 (+0000)
    Subject: fix setuid sometimes wouldn't
    
    4
    Commit: f1191b50ec11c8e2ca766d6d99eb5bb9d2c084a3
    Author: Al Viro <viro@zeniv.linux.org.uk>
    Date: Mon, 30 Mar 2009 11:35:18 +0000 (-0400)
    Subject: check_unsafe_exec() doesn't care about signal handlers sharing
    
    Signed-off-by: Hugh Dickins <hugh@veritas.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/compat.c
+++ fs/compat.c
@@ -1353,12 +1353,17 @@ int compat_do_execve(char * filename,
 {
 	struct linux_binprm *bprm;
 	struct file *file;
+	struct files_struct *displaced;
 	int retval;
+
+	retval = unshare_files(&displaced);
+	if (retval)
+		goto out_ret;
 
 	retval = -ENOMEM;
 	bprm = kzalloc(sizeof(*bprm), GFP_KERNEL);
 	if (!bprm)
-		goto out_ret;
+		goto out_files;
 
 	file = open_exec(filename);
 	retval = PTR_ERR(file);
@@ -1410,6 +1415,8 @@ int compat_do_execve(char * filename,
 		security_bprm_free(bprm);
 		acct_update_integrals(current);
 		free_bprm(bprm);
+		if (displaced)
+			put_files_struct(displaced);
 		return retval;
 	}
 
@@ -1430,6 +1437,9 @@ out_file:
 out_kfree:
 	free_bprm(bprm);
 
+out_files:
+	if (displaced)
+		reset_files_struct(displaced);
 out_ret:
 	return retval;
 }
