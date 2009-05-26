54823830733c96b9eda9e82167dcc6f04b8a4ca0
Jay Vosburgh <fubar@us.ibm.com>
2009-05-26 15:29:00 -0700

    bonding: fix alb mode locking regression
    [ Upstream commit 815bcc2719c12b6f5b511706e2d19728e07f0b02 ]
    
    Fix locking issue in alb MAC address management; removed
    incorrect locking and replaced with correct locking.  This bug was
    introduced in commit 059fe7a578fba5bbb0fdc0365bfcf6218fa25eb0
    ("bonding: Convert locks to _bh, rework alb locking for new locking")
    
    	Bug reported by Paul Smith <paul@mad-scientist.net>, who also
    tested the fix.
    
    Signed-off-by: Jay Vosburgh <fubar@us.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/bonding/bond_alb.c
+++ drivers/net/bonding/bond_alb.c
@@ -1716,9 +1716,6 @@ int bond_alb_set_mac_address(struct net_device *bond_dev, void *addr)
 		}
 	}
 
-	write_unlock_bh(&bond->curr_slave_lock);
-	read_unlock(&bond->lock);
-
 	if (swap_slave) {
 		alb_swap_mac_addr(bond, swap_slave, bond->curr_active_slave);
 		alb_fasten_mac_swap(bond, swap_slave, bond->curr_active_slave);
@@ -1726,15 +1723,14 @@ int bond_alb_set_mac_address(struct net_device *bond_dev, void *addr)
 		alb_set_slave_mac_addr(bond->curr_active_slave, bond_dev->dev_addr,
 				       bond->alb_info.rlb_enabled);
 
+		read_lock(&bond->lock);
 		alb_send_learning_packets(bond->curr_active_slave, bond_dev->dev_addr);
 		if (bond->alb_info.rlb_enabled) {
 			/* inform clients mac address has changed */
 			rlb_req_update_slave_clients(bond, bond->curr_active_slave);
 		}
+		read_unlock(&bond->lock);
 	}
-
-	read_lock(&bond->lock);
-	write_lock_bh(&bond->curr_slave_lock);
 
 	return 0;
 }
