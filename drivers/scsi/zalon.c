d9120d6d357e869bd8d064b38ac33a6b48ba36c9
James Bottomley <James.Bottomley@HansenPartnership.com>
2009-06-24 19:55:22 +0000

    SCSI: zalon: fix oops on attach failure
    commit d3a263a8168f78874254ea9da9595cfb0f3e96d7 upstream.
    
    I recently discovered on my zalon that if the attachment fails because
    of a bus misconfiguration (I scrapped my HVD array, so the card is now
    unterminated) then the system oopses.  The reason is that if
    ncr_attach() returns NULL (signalling failure) that NULL is passed by
    the goto failed straight into ncr_detach() which oopses.
    
    The fix is just to return -ENODEV in this case.
    
    Signed-off-by: James Bottomley <James.Bottomley@HansenPartnership.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/scsi/zalon.c
+++ drivers/scsi/zalon.c
@@ -134,7 +134,7 @@ zalon_probe(struct parisc_device *dev)
 
 	host = ncr_attach(&zalon7xx_template, unit, &device);
 	if (!host)
-		goto fail;
+		return -ENODEV;
 
 	if (request_irq(dev->irq, ncr53c8xx_intr, IRQF_SHARED, "zalon", host)) {
 		printk(KERN_ERR "%s: irq problem with %d, detaching\n ",
