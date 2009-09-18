d1dbc751eb2a4aaf628cc9dac6954e1bd1afc531
Marcelo Tosatti <mtosatti@redhat.com>
2009-09-18 20:08:05 -0300

    KVM: limit lapic periodic timer frequency
    (cherry picked from commit 1444885a045fe3b1905a14ea1b52540bf556578b)
    
    Otherwise its possible to starve the host by programming lapic timer
    with a very high frequency.
    
    Signed-off-by: Marcelo Tosatti <mtosatti@redhat.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -635,6 +635,15 @@ static void start_apic_timer(struct kvm_lapic *apic)
 
 	if (!apic->timer.period)
 		return;
+	/*
+	 * Do not allow the guest to program periodic timers with small
+	 * interval, since the hrtimers are not throttled by the host
+	 * scheduler.
+	 */
+	if (apic_lvtt_period(apic)) {
+		if (apic->timer.period < NSEC_PER_MSEC/2)
+			apic->timer.period = NSEC_PER_MSEC/2;
+	}
 
 	hrtimer_start(&apic->timer.dev,
 		      ktime_add_ns(now, apic->timer.period),
