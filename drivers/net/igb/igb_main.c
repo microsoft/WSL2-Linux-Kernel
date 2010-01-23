3b454d6b495fa853304741503db0b23f0542e3ef
Sridhar Samudrala <sri@us.ibm.com>
2010-01-23 02:02:21 -0800

    net: Fix IPv6 GSO type checks in Intel ethernet drivers
    commit 8e1e8a4779cb23c1d9f51e9223795e07ec54d77a upstream.
    
    Found this problem when testing IPv6 from a KVM guest to a remote
    host via e1000e device on the host.
    The following patch fixes the check for IPv6 GSO packet in Intel
    ethernet drivers to use skb_is_gso_v6(). SKB_GSO_DODGY is also set
    when packets are forwarded from a guest.
    
    Signed-off-by: Sridhar Samudrala <sri@us.ibm.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Faidon Liambotis <paravoid@debian.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/net/igb/igb_main.c
+++ drivers/net/igb/igb_main.c
@@ -3032,7 +3032,7 @@ static inline int igb_tso_adv(struct igb_adapter *adapter,
 							 iph->daddr, 0,
 							 IPPROTO_TCP,
 							 0);
-	} else if (skb_shinfo(skb)->gso_type == SKB_GSO_TCPV6) {
+	} else if (skb_is_gso_v6(skb)) {
 		ipv6_hdr(skb)->payload_len = 0;
 		tcp_hdr(skb)->check = ~csum_ipv6_magic(&ipv6_hdr(skb)->saddr,
 						       &ipv6_hdr(skb)->daddr,
