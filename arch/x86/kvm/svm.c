b6519334facc360b34e4e08639c3befceecd6c17
Joerg Roedel <joerg.roedel@amd.com>
2010-04-22 12:33:12 +0200

    KVM: SVM: Report emulated SVM features to userspace
    This patch implements the reporting of the emulated SVM
    features to userspace instead of the real hardware
    capabilities. Every real hardware capability needs emulation
    in nested svm so the old behavior was broken.
    
    Cc: stable@kernel.org
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Avi Kivity <avi@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>
    (cherry picked from commit c2c63a493924e09a1984d1374a0e60dfd54fc0b0)

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -2887,6 +2887,16 @@ static u64 svm_get_mt_mask(struct kvm_vcpu *vcpu, gfn_t gfn, bool is_mmio)
 
 static void svm_set_supported_cpuid(u32 func, struct kvm_cpuid_entry2 *entry)
 {
+	switch (func) {
+	case 0x8000000A:
+		entry->eax = 1; /* SVM revision 1 */
+		entry->ebx = 8; /* Lets support 8 ASIDs in case we add proper
+				   ASID emulation to nested SVM */
+		entry->ecx = 0; /* Reserved */
+		entry->edx = 0; /* Do not support any additional features */
+
+		break;
+	}
 }
 
 static const struct trace_print_flags svm_exit_reasons_str[] = {
