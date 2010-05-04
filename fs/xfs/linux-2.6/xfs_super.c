467e1b31ef815aadd8c7648929fd1fa8d46b811d
Dave Chinner <dchinner@redhat.com>
2010-05-04 12:58:20 +1000

    xfs: add a shrinker to background inode reclaim
    commit 9bf729c0af67897ea8498ce17c29b0683f7f2028 upstream
    
    On low memory boxes or those with highmem, kernel can OOM before the
    background reclaims inodes via xfssyncd. Add a shrinker to run inode
    reclaim so that it inode reclaim is expedited when memory is low.
    
    This is more complex than it needs to be because the VM folk don't
    want a context added to the shrinker infrastructure. Hence we need
    to add a global list of XFS mount structures so the shrinker can
    traverse them.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Acked-by: Alex Elder <aelder@sgi.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/xfs/linux-2.6/xfs_super.c
+++ fs/xfs/linux-2.6/xfs_super.c
@@ -1160,6 +1160,7 @@ xfs_fs_put_super(
 
 	xfs_unmountfs(mp);
 	xfs_freesb(mp);
+	xfs_inode_shrinker_unregister(mp);
 	xfs_icsb_destroy_counters(mp);
 	xfs_close_devices(mp);
 	xfs_dmops_put(mp);
@@ -1523,6 +1524,8 @@ xfs_fs_fill_super(
 	if (error)
 		goto fail_vnrele;
 
+	xfs_inode_shrinker_register(mp);
+
 	kfree(mtpt);
 	return 0;
 
@@ -1767,6 +1770,7 @@ init_xfs_fs(void)
 		goto out_cleanup_procfs;
 
 	vfs_initquota();
+	xfs_inode_shrinker_init();
 
 	error = register_filesystem(&xfs_fs_type);
 	if (error)
@@ -1794,6 +1798,7 @@ exit_xfs_fs(void)
 {
 	vfs_exitquota();
 	unregister_filesystem(&xfs_fs_type);
+	xfs_inode_shrinker_destroy();
 	xfs_sysctl_unregister();
 	xfs_cleanup_procfs();
 	xfs_buf_terminate();
