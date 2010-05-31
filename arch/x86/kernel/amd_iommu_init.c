0ac5ad9b26e8879728141ad76b56a8b9cc72cd98
Joerg Roedel <joerg.roedel@amd.com>
2010-05-31 15:05:20 +0200

    x86/amd-iommu: Fall back to GART if initialization fails
    commit d7f0776975334070a93370ae048fda0c31a91c38 upstream.
    
    This patch implements a fallback to the GART IOMMU if this
    is possible and the AMD IOMMU initialization failed.
    Otherwise the fallback would be nommu which is very
    problematic on machines with more than 4GB of memory or
    swiotlb which hurts io-performance.
    
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/kernel/amd_iommu_init.c
+++ arch/x86/kernel/amd_iommu_init.c
@@ -1340,6 +1340,15 @@ free:
 
 	free_unity_maps();
 
+#ifdef CONFIG_GART_IOMMU
+	/*
+	 * We failed to initialize the AMD IOMMU - try fallback to GART
+	 * if possible.
+	 */
+	gart_iommu_init();
+
+#endif
+
 	goto out;
 }
 
