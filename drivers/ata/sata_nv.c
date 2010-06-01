a4fd5acd21386fe469c4815e37c963562041a615
Tejun Heo <tj@kernel.org>
2010-06-01 17:29:21 +0200

    sata_nv: don't diddle with nIEN on mcp55
    commit f3faf8fc3fab45c3526efe8c9e99bb23f8723350 upstream.
    
    On mcp55, nIEN gets stuck once set and liteon blueray rom iHOS104-08
    violates ATA specification and fails to set I on D2H Reg FIS if nIEN
    is set when the command was issued.  When the other party is following
    the spec, both devices can work fine but when the two flaws are put
    together, they can't talk to each other.
    
    mcp55 has its own IRQ masking mechanism and there's no reason to mess
    with nIEN in the first place.  Fix it by dropping nIEN diddling from
    nv_mcp55_freeze/thaw().
    
    This was originally reported by Cengiz.  Although Cengiz hasn't
    verified the fix yet, I could reproduce this problem and verfiy the
    fix.  Even if Cengiz is experiencing different or additional problems,
    this patch is needed.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Cengiz Günay <cgunay@emory.edu>
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/ata/sata_nv.c
+++ drivers/ata/sata_nv.c
@@ -1674,7 +1674,6 @@ static void nv_mcp55_freeze(struct ata_port *ap)
 	mask = readl(mmio_base + NV_INT_ENABLE_MCP55);
 	mask &= ~(NV_INT_ALL_MCP55 << shift);
 	writel(mask, mmio_base + NV_INT_ENABLE_MCP55);
-	ata_sff_freeze(ap);
 }
 
 static void nv_mcp55_thaw(struct ata_port *ap)
@@ -1688,7 +1687,6 @@ static void nv_mcp55_thaw(struct ata_port *ap)
 	mask = readl(mmio_base + NV_INT_ENABLE_MCP55);
 	mask |= (NV_INT_MASK_MCP55 << shift);
 	writel(mask, mmio_base + NV_INT_ENABLE_MCP55);
-	ata_sff_thaw(ap);
 }
 
 static void nv_adma_error_handler(struct ata_port *ap)
