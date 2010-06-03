18ba8e2c21b721d1c36e65525a30b25804e9b33e
Daniel Mack <daniel@caiaq.de>
2010-06-03 13:55:02 +0200

    USB: ftdi_sio: fix DTR/RTS line modes
    commit 6a1a82df91fa0eb1cc76069a9efe5714d087eccd upstream.
    
    Call set_mctrl() and clear_mctrl() according to the flow control mode
    selected. This makes serial communication for FT232 connected devices
    work when CRTSCTS is not set.
    
    This fixes a regression introduced by 4175f3e31 ("tty_port: If we are
    opened non blocking we still need to raise the carrier"). This patch
    calls the low-level driver's dtr_rts() function which consequently sets
    TIOCM_DTR | TIOCM_RTS. A later call to set_termios() without CRTSCTS in
    cflags, however, does not reset these bits, and so data is not actually
    sent out on the serial wire.
    
    Signed-off-by: Daniel Mack <daniel@caiaq.de>
    Cc: Johan Hovold <jhovold@gmail.com>
    Cc: Alan Cox <alan@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/serial/ftdi_sio.c
+++ drivers/usb/serial/ftdi_sio.c
@@ -2312,6 +2312,8 @@ static void ftdi_set_termios(struct tty_struct *tty,
 				"urb failed to set to rts/cts flow control\n");
 		}
 
+		/* raise DTR/RTS */
+		set_mctrl(port, TIOCM_DTR | TIOCM_RTS);
 	} else {
 		/*
 		 * Xon/Xoff code
@@ -2359,6 +2361,8 @@ static void ftdi_set_termios(struct tty_struct *tty,
 			}
 		}
 
+		/* lower DTR/RTS */
+		clear_mctrl(port, TIOCM_DTR | TIOCM_RTS);
 	}
 	return;
 }
