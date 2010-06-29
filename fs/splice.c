abce205833f32d215c65d1e223d6d898647fbcad
Changli Gao <xiaosuo@gmail.com>
2010-06-29 13:09:18 +0200

    splice: direct_splice_actor() should not use pos in sd
    commit 2cb4b05e7647891b46b91c07c9a60304803d1688 upstream.
    
    direct_splice_actor() shouldn't use sd->pos, as sd->pos is for file reading,
    file->f_pos should be used instead.
    
    Signed-off-by: Changli Gao <xiaosuo@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/splice.c
+++ fs/splice.c
@@ -1231,7 +1231,8 @@ static int direct_splice_actor(struct pipe_inode_info *pipe,
 {
 	struct file *file = sd->u.file;
 
-	return do_splice_from(pipe, file, &sd->pos, sd->total_len, sd->flags);
+	return do_splice_from(pipe, file, &file->f_pos, sd->total_len,
+			      sd->flags);
 }
 
 /**
