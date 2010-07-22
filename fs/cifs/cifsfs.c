1180deda29be7691126b9bdd608d45d3678b7875
David Howells <dhowells@redhat.com>
2010-07-22 12:53:18 +0100

    CIFS: Fix a malicious redirect problem in the DNS lookup code
    commit 4c0c03ca54f72fdd5912516ad0a23ec5cf01bda7 upstream.
    
    Fix the security problem in the CIFS filesystem DNS lookup code in which a
    malicious redirect could be installed by a random user by simply adding a
    result record into one of their keyrings with add_key() and then invoking a
    CIFS CFS lookup [CVE-2010-2524].
    
    This is done by creating an internal keyring specifically for the caching of
    DNS lookups.  To enforce the use of this keyring, the module init routine
    creates a set of override credentials with the keyring installed as the thread
    keyring and instructs request_key() to only install lookup result keys in that
    keyring.
    
    The override is then applied around the call to request_key().
    
    This has some additional benefits when a kernel service uses this module to
    request a key:
    
     (1) The result keys are owned by root, not the user that caused the lookup.
    
     (2) The result keys don't pop up in the user's keyrings.
    
     (3) The result keys don't come out of the quota of the user that caused the
         lookup.
    
    The keyring can be viewed as root by doing cat /proc/keys:
    
    2a0ca6c3 I-----     1 perm 1f030000     0     0 keyring   .dns_resolver: 1/4
    
    It can then be listed with 'keyctl list' by root.
    
    	# keyctl list 0x2a0ca6c3
    	1 key in keyring:
    	726766307: --alswrv     0     0 dns_resolver: foo.bar.com
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    Reviewed-and-Tested-by: Jeff Layton <jlayton@redhat.com>
    Acked-by: Steve French <smfrench@gmail.com>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- fs/cifs/cifsfs.c
+++ fs/cifs/cifsfs.c
@@ -1046,7 +1046,7 @@ init_cifs(void)
 		goto out_unregister_filesystem;
 #endif
 #ifdef CONFIG_CIFS_DFS_UPCALL
-	rc = register_key_type(&key_type_dns_resolver);
+	rc = cifs_init_dns_resolver();
 	if (rc)
 		goto out_unregister_key_type;
 #endif
@@ -1058,7 +1058,7 @@ init_cifs(void)
 
  out_unregister_resolver_key:
 #ifdef CONFIG_CIFS_DFS_UPCALL
-	unregister_key_type(&key_type_dns_resolver);
+	cifs_exit_dns_resolver();
  out_unregister_key_type:
 #endif
 #ifdef CONFIG_CIFS_UPCALL
@@ -1084,7 +1084,7 @@ exit_cifs(void)
 	cifs_proc_clean();
 #ifdef CONFIG_CIFS_DFS_UPCALL
 	cifs_dfs_release_automount_timer();
-	unregister_key_type(&key_type_dns_resolver);
+	cifs_exit_dns_resolver();
 #endif
 #ifdef CONFIG_CIFS_UPCALL
 	unregister_key_type(&cifs_spnego_key_type);
