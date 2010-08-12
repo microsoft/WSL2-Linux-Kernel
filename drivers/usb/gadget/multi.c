5aaade9e6a6f829ae8d94e5e0b04897ae9d4535e
Michal Nazarewicz <m.nazarewicz@samsung.com>
2010-08-12 17:43:44 +0200

    USB: gadget: g_multi: fixed vendor and product ID
    commit 1c6529e92b7682573837e9c9eb7b5ba7a8216a88 upstream.
    
    This patch fixes the vendor and product ID the gadget uses
    by replacing the temporary IDs that were used during
    development (which should never get into mainline) with
    proper IDs.
    
    Signed-off-by: Michal Nazarewicz <m.nazarewicz@samsung.com>
    Signed-off-by: Kyungmin Park <kyungmin.park@samsung.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/gadget/multi.c
+++ drivers/usb/gadget/multi.c
@@ -39,8 +39,8 @@
 
 /*-------------------------------------------------------------------------*/
 
-#define MULTI_VENDOR_NUM	0x0525	/* XXX NetChip */
-#define MULTI_PRODUCT_NUM	0xa4ab	/* XXX */
+#define MULTI_VENDOR_NUM	0x1d6b	/* Linux Foundation */
+#define MULTI_PRODUCT_NUM	0x0104	/* Multifunction Composite Gadget */
 
 /*-------------------------------------------------------------------------*/
 
