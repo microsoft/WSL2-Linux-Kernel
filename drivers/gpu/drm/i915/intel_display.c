2a0726e890af8f37f039cbec69b7c4f422e5ddf7
Chris Wilson <chris@chris-wilson.co.uk>
2010-09-06 16:17:22 +0100

    drm/i915: Prevent double dpms on
    commit 032d2a0d068b0368296a56469761394ef03207c3 upstream.
    
    Arguably this is a bug in drm-core in that we should not be called twice
    in succession with DPMS_ON, however this is still occuring and we see
    FDI link training failures on the second call leading to the occassional
    blank display. For the time being ignore the repeated call.
    
    Original patch by Dave Airlie <airlied@redhat.com>
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -2314,6 +2314,9 @@ static void intel_crtc_dpms(struct drm_crtc *crtc, int mode)
 	int pipe = intel_crtc->pipe;
 	bool enabled;
 
+	if (intel_crtc->dpms_mode == mode)
+		return;
+
 	dev_priv->display.dpms(crtc, mode);
 
 	intel_crtc->dpms_mode = mode;
@@ -4915,7 +4918,7 @@ static void intel_crtc_init(struct drm_device *dev, int pipe)
 	dev_priv->pipe_to_crtc_mapping[intel_crtc->pipe] = &intel_crtc->base;
 
 	intel_crtc->cursor_addr = 0;
-	intel_crtc->dpms_mode = DRM_MODE_DPMS_OFF;
+	intel_crtc->dpms_mode = -1;
 	drm_crtc_helper_add(&intel_crtc->base, &intel_helper_funcs);
 
 	intel_crtc->busy = false;
