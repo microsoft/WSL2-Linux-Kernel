e1d7519864aede29b957d1dc5760d83d9ec2ff3a
Chris Wilson <chris@chris-wilson.co.uk>
2010-09-17 08:22:30 +0100

    drm/i915,agp/intel: Add second set of PCI-IDs for B43
    commit 41a51428916ab04587bacee2dda61c4a0c4fc02f upstream.
    
    There is a second revision of B43 (a desktop gen4 part) floating around,
    functionally equivalent to the original B43, so simply add the new
    PCI-IDs.
    
    Bugzilla: https://bugs.freedesktop.org/show_bugs.cgi?id=30221
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/gpu/drm/i915/i915_drv.c
+++ drivers/gpu/drm/i915/i915_drv.c
@@ -175,6 +175,7 @@ static const struct pci_device_id pciidlist[] = {
 	INTEL_VGA_DEVICE(0x2e22, &intel_g45_info),
 	INTEL_VGA_DEVICE(0x2e32, &intel_g45_info),
 	INTEL_VGA_DEVICE(0x2e42, &intel_g45_info),
+	INTEL_VGA_DEVICE(0x2e92, &intel_g45_info),		/* B43_G.1 */
 	INTEL_VGA_DEVICE(0xa001, &intel_pineview_info),
 	INTEL_VGA_DEVICE(0xa011, &intel_pineview_info),
 	INTEL_VGA_DEVICE(0x0042, &intel_ironlake_d_info),
