92ff7352975cc5e700e853a8eec15a328a46267f
Greg Kroah-Hartman <gregkh@suse.de>
2010-11-15 11:11:45 -0800

    USB: atm: ueagle-atm: fix up some permissions on the sysfs files
    commit e502ac5e1eca99d7dc3f12b2a6780ccbca674858 upstream.
    
    Some of the sysfs files had the incorrect permissions.  Some didn't make
    sense at all (writable for a file that you could not write to?)
    
    Reported-by: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Matthieu Castet <castet.matthieu@free.fr>
    Cc: Stanislaw Gruszka <stf_xl@wp.pl>
    Cc: Damien Bergamini <damien.bergamini@free.fr>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/usb/atm/ueagle-atm.c
+++ drivers/usb/atm/ueagle-atm.c
@@ -2260,7 +2260,7 @@ out:
 	return ret;
 }
 
-static DEVICE_ATTR(stat_status, S_IWUGO | S_IRUGO, read_status, reboot);
+static DEVICE_ATTR(stat_status, S_IWUSR | S_IRUGO, read_status, reboot);
 
 static ssize_t read_human_status(struct device *dev, struct device_attribute *attr,
 		char *buf)
@@ -2323,7 +2323,7 @@ out:
 	return ret;
 }
 
-static DEVICE_ATTR(stat_human_status, S_IWUGO | S_IRUGO, read_human_status, NULL);
+static DEVICE_ATTR(stat_human_status, S_IRUGO, read_human_status, NULL);
 
 static ssize_t read_delin(struct device *dev, struct device_attribute *attr,
 		char *buf)
@@ -2355,7 +2355,7 @@ out:
 	return ret;
 }
 
-static DEVICE_ATTR(stat_delin, S_IWUGO | S_IRUGO, read_delin, NULL);
+static DEVICE_ATTR(stat_delin, S_IRUGO, read_delin, NULL);
 
 #define UEA_ATTR(name, reset) 					\
 								\
