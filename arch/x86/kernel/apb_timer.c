8cc8e3f986fcb39f707e32f68d12a896f673dba0
Jacob Pan <jacob.jun.pan@linux.intel.com>
2011-02-17 16:58:21 -0800

    x86, mrst: Set correct APB timer IRQ affinity for secondary cpu
    This is a backport for 2.6.37 stable. The original commit ID is
    6550904ddbc3c286798a87edf95eeebcc62bc58a
    
    Offlining the secondary CPU causes the timer irq affinity to be set to
    CPU 0. When the secondary CPU is back online again, the wrong irq
    affinity will be used.
    
    This patch ensures secondary per CPU timer always has the correct
    IRQ affinity when enabled.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    LKML-Reference: <1294963604-18111-1-git-send-email-jacob.jun.pan@linux.intel.com>
    Signed-off-by: H. Peter Anvin <hpa@linux.intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- arch/x86/kernel/apb_timer.c
+++ arch/x86/kernel/apb_timer.c
@@ -313,10 +313,12 @@ static void apbt_setup_irq(struct apbt_dev *adev)
 	if (adev->irq == 0)
 		return;
 
+	irq_modify_status(adev->irq, 0, IRQ_MOVE_PCNTXT);
+	irq_set_affinity(adev->irq, cpumask_of(adev->cpu));
+	/* APB timer irqs are set up as mp_irqs, timer is edge type */
+	__set_irq_handler(adev->irq, handle_edge_irq, 0, "edge");
+
 	if (system_state == SYSTEM_BOOTING) {
-		irq_modify_status(adev->irq, 0, IRQ_MOVE_PCNTXT);
-		/* APB timer irqs are set up as mp_irqs, timer is edge type */
-		__set_irq_handler(adev->irq, handle_edge_irq, 0, "edge");
 		if (request_irq(adev->irq, apbt_interrupt_handler,
 				IRQF_TIMER | IRQF_DISABLED | IRQF_NOBALANCING,
 				adev->name, adev)) {
