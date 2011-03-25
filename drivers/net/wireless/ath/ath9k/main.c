0ed05379c28294629c08fa348c83965e098bbcf6
Felix Fietkau <nbd@openwrt.org>
2011-03-25 17:43:41 +0100

    ath9k: fix a chip wakeup related crash in ath9k_start
    [ upstream commit f62d816fc4324afbb7cf90110c70b6a14139b225 ]
    
    When the chip is still asleep when ath9k_start is called,
    ath9k_hw_configpcipowersave can trigger a data bus error.
    
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Cc: stable@kernel.org
    Signed-off-by: John W. Linville <linville@tuxdriver.com>

--- drivers/net/wireless/ath/ath9k/main.c
+++ drivers/net/wireless/ath/ath9k/main.c
@@ -1124,6 +1124,8 @@ static int ath9k_start(struct ieee80211_hw *hw)
 		  "Starting driver with initial channel: %d MHz\n",
 		  curchan->center_freq);
 
+	ath9k_ps_wakeup(sc);
+
 	mutex_lock(&sc->mutex);
 
 	if (ath9k_wiphy_started(sc)) {
@@ -1237,6 +1239,8 @@ static int ath9k_start(struct ieee80211_hw *hw)
 
 mutex_unlock:
 	mutex_unlock(&sc->mutex);
+
+	ath9k_ps_restore(sc);
 
 	return r;
 }
