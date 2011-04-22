e165f370fe28e09a5a1ad1acc250f8b5b7f72c22
David S. Miller <davem@davemloft.net>
2011-04-21 21:17:25 -0700

    Revert "bridge: Forward reserved group addresses if !STP"
    [ Upstream commit f01cb5fbea1c1613621f9f32f385e12c1a29dde0 ]
    
    This reverts commit 1e253c3b8a1aeed51eef6fc366812f219b97de65.
    
    It breaks 802.3ad bonding inside of a bridge.
    
    The commit was meant to support transport bridging, and specifically
    virtual machines bridged to an ethernet interface connected to a
    switch port wiht 802.1x enabled.
    
    But this isn't the way to do it, it breaks too many other things.
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- net/bridge/br_input.c
+++ net/bridge/br_input.c
@@ -163,7 +163,7 @@ struct sk_buff *br_handle_frame(struct sk_buff *skb)
 			goto drop;
 
 		/* If STP is turned off, then forward */
-		if (p->br->stp_enabled == BR_NO_STP)
+		if (p->br->stp_enabled == BR_NO_STP && dest[5] == 0)
 			goto forward;
 
 		if (NF_HOOK(NFPROTO_BRIDGE, NF_BR_LOCAL_IN, skb, skb->dev,
