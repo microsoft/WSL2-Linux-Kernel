cf72dbd757c58e568df27c1d6b8c96e0642b8384
Jens Axboe <jaxboe@fusionio.com>
2011-05-27 07:44:43 +0200

    block: export blk_{get,put}_queue()
    commit d86e0e83b32bc84600adb0b6ea1fce389b266682 upstream.
    
    We need them in SCSI to fix a bug, but currently they are not
    exported to modules. Export them.
    
    Signed-off-by: Jens Axboe <jaxboe@fusionio.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -439,6 +439,7 @@ void blk_put_queue(struct request_queue *q)
 {
 	kobject_put(&q->kobj);
 }
+EXPORT_SYMBOL(blk_put_queue);
 
 void blk_cleanup_queue(struct request_queue *q)
 {
@@ -612,6 +613,7 @@ int blk_get_queue(struct request_queue *q)
 
 	return 1;
 }
+EXPORT_SYMBOL(blk_get_queue);
 
 static inline void blk_free_request(struct request_queue *q, struct request *rq)
 {
