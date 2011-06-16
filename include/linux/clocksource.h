b63010f54cdcb456c3a29e242a0769e5b412d785
Thomas Gleixner <tglx@linutronix.de>
2011-06-16 16:22:08 +0200

    clocksource: Make watchdog robust vs. interruption
    commit b5199515c25cca622495eb9c6a8a1d275e775088 upstream.
    
    The clocksource watchdog code is interruptible and it has been
    observed that this can trigger false positives which disable the TSC.
    
    The reason is that an interrupt storm or a long running interrupt
    handler between the read of the watchdog source and the read of the
    TSC brings the two far enough apart that the delta is larger than the
    unstable treshold. Move both reads into a short interrupt disabled
    region to avoid that.
    
    Reported-and-tested-by: Vernon Mauery <vernux@us.ibm.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- include/linux/clocksource.h
+++ include/linux/clocksource.h
@@ -190,6 +190,7 @@ struct clocksource {
 #ifdef CONFIG_CLOCKSOURCE_WATCHDOG
 	/* Watchdog related data, used by the framework */
 	struct list_head wd_list;
+	cycle_t cs_last;
 	cycle_t wd_last;
 #endif
 };
