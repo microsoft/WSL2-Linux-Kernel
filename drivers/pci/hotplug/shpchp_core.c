f8b35bffd3cc4ffd14ab9c07909cdc45a9006776
Bjorn Helgaas <bhelgaas@google.com>
2011-08-23 10:16:43 -0600

    PCI hotplug: shpchp: don't blindly claim non-AMD 0x7450 device IDs
    commit 4cac2eb158c6da0c761689345c6cc5df788a6292 upstream.
    
    Previously we claimed device ID 0x7450, regardless of the vendor, which is
    clearly wrong.  Now we'll claim that device ID only for AMD.
    
    I suspect this was just a typo in the original code, but it's possible this
    change will break shpchp on non-7450 AMD bridges.  If so, we'll have to fix
    them as we find them.
    
    Reference: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=638863
    Reported-by: Ralf Jung <ralfjung-e@gmx.de>
    Cc: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/pci/hotplug/shpchp_core.c
+++ drivers/pci/hotplug/shpchp_core.c
@@ -312,8 +312,8 @@ static int get_cur_bus_speed (struct hotplug_slot *hotplug_slot, enum pci_bus_sp
 
 static int is_shpc_capable(struct pci_dev *dev)
 {
-	if ((dev->vendor == PCI_VENDOR_ID_AMD) || (dev->device ==
-						PCI_DEVICE_ID_AMD_GOLAM_7450))
+	if (dev->vendor == PCI_VENDOR_ID_AMD &&
+	    dev->device == PCI_DEVICE_ID_AMD_GOLAM_7450)
 		return 1;
 	if (!pci_find_capability(dev, PCI_CAP_ID_SHPC))
 		return 0;
