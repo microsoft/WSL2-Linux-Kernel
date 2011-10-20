425f213726d314c0820a63cac1389d19a76df37f
Manoj Iyer <manoj.iyer@canonical.com>
2011-10-20 20:50:25 +0200

    thinkpad-acpi: module autoloading for newer Lenovo ThinkPads.
    commit 9fbdaeb4f4dd14a0caa9fc35c496d5440c251a3a upstream.
    
    The newer Lenovo ThinkPads have HKEY HID of LEN0068 instead
    of IBM0068. Added new HID so that thinkpad_acpi module will
    auto load on these newer Lenovo ThinkPads.
    
    Acked-by: Henrique de Moraes Holschuh <hmh@hmh.eng.br>
    Cc: stable@vger.kernel.org
    Signed-off-by: Manoj Iyer <manoj.iyer@canonical.com>
    Signed-off-by: Andy Lutomirski <luto@mit.edu>
    Signed-off-by: Matthew Garrett <mjg@redhat.com>
    Signed-off-by: Bjørn Mork <bjorn@mork.no>
    Signed-off-by: Greg Kroah-Hartman <gregkh@suse.de>

--- drivers/platform/x86/thinkpad_acpi.c
+++ drivers/platform/x86/thinkpad_acpi.c
@@ -122,7 +122,9 @@ enum {
 };
 
 /* ACPI HIDs */
-#define TPACPI_ACPI_HKEY_HID		"IBM0068"
+#define TPACPI_ACPI_IBM_HKEY_HID	"IBM0068"
+#define TPACPI_ACPI_LENOVO_HKEY_HID	"LEN0068"
+#define TPACPI_ACPI_EC_HID		"PNP0C09"
 
 /* Input IDs */
 #define TPACPI_HKEY_INPUT_PRODUCT	0x5054 /* "TP" */
@@ -3849,7 +3851,8 @@ errexit:
 }
 
 static const struct acpi_device_id ibm_htk_device_ids[] = {
-	{TPACPI_ACPI_HKEY_HID, 0},
+	{TPACPI_ACPI_IBM_HKEY_HID, 0},
+	{TPACPI_ACPI_LENOVO_HKEY_HID, 0},
 	{"", 0},
 };
 
