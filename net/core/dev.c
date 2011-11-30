e4a0eea634767f19bdeb8c7044946c4a7ded542c
Benjamin Poirier <bpoirier@suse.de>
2011-11-30 07:47:18 -0500

    gro: reset vlan_tci on reuse
    This one liner is part of upstream
    commit 3701e51382a026cba10c60b03efabe534fba4ca4
    Author: Jesse Gross <jesse@nicira.com>
    
        vlan: Centralize handling of hardware acceleration.
    
    The bulk of that commit is a rework of the hardware assisted vlan tagging
    driver interface, and as such doesn't classify for -stable inclusion. The fix
    that is needed is a part of that commit but can work independently of the
    rest.
    
    This patch can avoid panics on the 2.6.32.y -stable kernels and is in the same
    spirit as mainline commits
    66c46d7 gro: Reset dev pointer on reuse
    6d152e2 gro: reset skb_iif on reuse
    which are already in -stable.
    
    For drivers using the vlan_gro_frags() interface, a packet with an invalid tci
    leads to GRO_DROP and napi_reuse_skb(). The skb has to be sanitized before
    being reused or we may send an skb with an invalid vlan_tci field up the stack
    where it is not expected.
    
    Signed-off-by: Benjamin Poirier <bpoirier@suse.de>
    Cc: Jesse Gross <jesse@nicira.com>
    Acked-by: David S. Miller <davem@davemloft.net>
    [PG: taken from v2.6.32.y stable, commit 5aff28abc7e]
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -2823,6 +2823,7 @@ void napi_reuse_skb(struct napi_struct *napi, struct sk_buff *skb)
 {
 	__skb_pull(skb, skb_headlen(skb));
 	skb_reserve(skb, NET_IP_ALIGN - skb_headroom(skb));
+	skb->vlan_tci = 0;
 	skb->dev = napi->dev;
 	skb->skb_iif = 0;
 
