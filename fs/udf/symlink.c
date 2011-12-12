8b5c9cbf33c4ef5a8091c4db1b9fc9010d5f699e
Jan Kara <jack@suse.cz>
2011-12-12 15:13:50 +0100

    udf: Treat symlink component of type 2 as /
    commit fef2e9f3301934773e4f1b3cc5c7bffb119346b8 upstream.
    
    Currently, we ignore symlink component of type 2. But mkisofs and other OS'
    seem to treat it as / so do the same for compatibility.
    
    Reported-by: "Gábor S." <otnaccess@hotmail.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    [bwh: Needed for the following fix]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- fs/udf/symlink.c
+++ fs/udf/symlink.c
@@ -43,10 +43,16 @@ static void udf_pc_to_char(struct super_block *sb, char *from, int fromlen,
 		pc = (struct pathComponent *)(from + elen);
 		switch (pc->componentType) {
 		case 1:
-			if (pc->lengthComponentIdent == 0) {
-				p = to;
-				*p++ = '/';
-			}
+			/*
+			 * Symlink points to some place which should be agreed
+ 			 * upon between originator and receiver of the media. Ignore.
+			 */
+			if (pc->lengthComponentIdent > 0)
+				break;
+			/* Fall through */
+		case 2:
+			p = to;
+			*p++ = '/';
 			break;
 		case 3:
 			memcpy(p, "../", 3);
