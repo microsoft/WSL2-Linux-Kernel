d67f05eb15321bcfaa62d544a0f11d0015736caf
Eric Dumazet <eric.dumazet@gmail.com>
2012-02-23 10:55:02 +0000

    ipsec: be careful of non existing mac headers
    [ Upstream commit 03606895cd98c0a628b17324fd7b5ff15db7e3cd ]
    
    Niccolo Belli reported ipsec crashes in case we handle a frame without
    mac header (atm in his case)
    
    Before copying mac header, better make sure it is present.
    
    Bugzilla reference:  https://bugzilla.kernel.org/show_bug.cgi?id=42809
    
    Reported-by: Niccolò Belli <darkbasic@linuxsystems.it>
    Tested-by: Niccolò Belli <darkbasic@linuxsystems.it>
    Signed-off-by: Eric Dumazet <eric.dumazet@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Willy Tarreau <w@1wt.eu>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1312,6 +1312,16 @@ static inline void skb_set_mac_header(struct sk_buff *skb, const int offset)
 }
 #endif /* NET_SKBUFF_DATA_USES_OFFSET */
 
+static inline void skb_mac_header_rebuild(struct sk_buff *skb)
+{
+	if (skb_mac_header_was_set(skb)) {
+		const unsigned char *old_mac = skb_mac_header(skb);
+
+		skb_set_mac_header(skb, -skb->mac_len);
+		memmove(skb_mac_header(skb), old_mac, skb->mac_len);
+	}
+}
+
 static inline int skb_transport_offset(const struct sk_buff *skb)
 {
 	return skb_transport_header(skb) - skb->data;
