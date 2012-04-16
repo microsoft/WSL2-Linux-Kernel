96f1ffe01afd835e18c6e0a67896376061b33d9d
Chris Wilson <chris@chris-wilson.co.uk>
2012-04-16 15:16:42 +0100

    drm/i915: Hold mode_config lock whilst changing mode for lastclose()
    commit c291be9dba370ba696a0d482249a212cf5c15f45 upstream.
    
    Upon lastclose(), we switch back to the fbcon configuration. This
    requires taking the mode_config lock in order to serialise the change
    with output probing elsewhere.
    
    Reported-by: Oleksij Rempel <bug-track@fisher-privat.net>
    References: https://bugs.freedesktop.org/show_bug.cgi?id=48652
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Acked-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-Off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_fb.c
+++ drivers/gpu/drm/i915/intel_fb.c
@@ -283,6 +283,8 @@ void intel_fb_restore_mode(struct drm_device *dev)
 	struct drm_mode_config *config = &dev->mode_config;
 	struct drm_plane *plane;
 
+	mutex_lock(&dev->mode_config.mutex);
+
 	ret = drm_fb_helper_restore_fbdev_mode(&dev_priv->fbdev->helper);
 	if (ret)
 		DRM_DEBUG("failed to restore crtc mode\n");
@@ -290,4 +292,6 @@ void intel_fb_restore_mode(struct drm_device *dev)
 	/* Be sure to shut off any planes that may be active */
 	list_for_each_entry(plane, &config->plane_list, head)
 		plane->funcs->disable_plane(plane);
+
+	mutex_unlock(&dev->mode_config.mutex);
 }
