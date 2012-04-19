2044034adba9a93f2d9f128ea60958ae67993649
Alex Deucher <alexander.deucher@amd.com>
2012-04-19 10:48:38 -0400

    drm/radeon/kms: use frac fb div on APUs
    commit 37d4174d2d252c37dcb3d88cafae488542087848 upstream.
    
    Seems to be more stable on certain monitors.
    
    Fixes:
    https://bugs.freedesktop.org/show_bug.cgi?id=48880
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    [bwh: Backported to 3.2: drop the ASIC_IS_DCE61() case]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/gpu/drm/radeon/atombios_crtc.c
+++ drivers/gpu/drm/radeon/atombios_crtc.c
@@ -541,6 +541,9 @@ static u32 atombios_adjust_pll(struct drm_crtc *crtc,
 
 		if (rdev->family < CHIP_RV770)
 			pll->flags |= RADEON_PLL_PREFER_MINM_OVER_MAXP;
+		/* use frac fb div on APUs */
+		if (ASIC_IS_DCE41(rdev))
+			pll->flags |= RADEON_PLL_USE_FRAC_FB_DIV;
 	} else {
 		pll->flags |= RADEON_PLL_LEGACY;
 
