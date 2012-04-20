b6eb7a21211e409aba13e14f941d512dd8b5de33
Arun Sharma <asharma@fb.com>
2012-04-20 15:41:35 -0700

    perf/x86: Check if user fp is valid
    commit bc6ca7b342d5ae15c3ba3081fd40271b8039fb25 upstream.
    
    Signed-off-by: Arun Sharma <asharma@fb.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/r/1334961696-19580-4-git-send-email-asharma@fb.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [bwh: Backported to 3.2: also add user_addr_max() macro]
    Cc: Jann Horn <jannh@google.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- arch/x86/kernel/cpu/perf_event.c
+++ arch/x86/kernel/cpu/perf_event.c
@@ -1461,6 +1461,12 @@ perf_callchain_kernel(struct perf_callchain_entry *entry, struct pt_regs *regs)
 	dump_trace(NULL, regs, NULL, 0, &backtrace_ops, entry);
 }
 
+static inline int
+valid_user_frame(const void __user *fp, unsigned long size)
+{
+	return (__range_not_ok(fp, size, TASK_SIZE) == 0);
+}
+
 #ifdef CONFIG_COMPAT
 static inline int
 perf_callchain_user32(struct pt_regs *regs, struct perf_callchain_entry *entry)
@@ -1483,6 +1489,9 @@ perf_callchain_user32(struct pt_regs *regs, struct perf_callchain_entry *entry)
 			break;
 
 		if (fp < compat_ptr(regs->sp))
+			break;
+
+		if (!valid_user_frame(fp, sizeof(frame)))
 			break;
 
 		perf_callchain_store(entry, frame.return_address);
@@ -1529,6 +1538,9 @@ perf_callchain_user(struct perf_callchain_entry *entry, struct pt_regs *regs)
 			break;
 
 		if ((unsigned long)fp < regs->sp)
+			break;
+
+		if (!valid_user_frame(fp, sizeof(frame)))
 			break;
 
 		perf_callchain_store(entry, frame.return_address);
