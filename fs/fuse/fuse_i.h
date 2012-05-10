bcbb4d014a85dcbb499b9a9cfcfbd9f779e44edf
Pavel Shilovsky <piastry@etersoft.ru>
2012-05-10 19:49:38 +0400

    fuse: fix stat call on 32 bit platforms
    commit 45c72cd73c788dd18c8113d4a404d6b4a01decf1 upstream.
    
    Now we store attr->ino at inode->i_ino, return attr->ino at the
    first time and then return inode->i_ino if the attribute timeout
    isn't expired. That's wrong on 32 bit platforms because attr->ino
    is 64 bit and inode->i_ino is 32 bit in this case.
    
    Fix this by saving 64 bit ino in fuse_inode structure and returning
    it every time we call getattr. Also squash attr->ino into inode->i_ino
    explicitly.
    
    Signed-off-by: Pavel Shilovsky <piastry@etersoft.ru>
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -76,6 +76,9 @@ struct fuse_inode {
 	    preserve the original mode */
 	mode_t orig_i_mode;
 
+	/** 64 bit inode number */
+	u64 orig_ino;
+
 	/** Version of last attribute change */
 	u64 attr_version;
 
