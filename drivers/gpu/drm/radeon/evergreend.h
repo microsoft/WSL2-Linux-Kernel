bdaa64973474612383fc8f144646174ddcfd9db6
Alex Deucher <alexander.deucher@amd.com>
2012-08-15 17:18:42 -0400

    drm/radeon: properly handle mc_stop/mc_resume on evergreen+ (v2)
    commit 62444b7462a2b98bc78d68736c03a7c4e66ba7e2 upstream.
    
    - Stop the displays from accessing the FB
    - Block CPU access
    - Turn off MC client access
    
    This should fix issues some users have seen, especially
    with UEFI, when changing the MC FB location that result
    in hangs or display corruption.
    
    v2: fix crtc enabled check noticed by Luca Tettamanti
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    [bwh: Backported to 3.2:
     - Drop DCE6 cases
     - Call evergreen_mc_wait_for_idle() directly
     - Add dce4_wait_for_vblank() (commits 3ae19b750bdc09ce233e1504348320141593ffda
       and 4a15903db02026728d0cf2755c6fabae16b8db6a) and call it directly
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/gpu/drm/radeon/evergreend.h
+++ drivers/gpu/drm/radeon/evergreend.h
@@ -77,6 +77,10 @@
 
 #define	CONFIG_MEMSIZE					0x5428
 
+#define	BIF_FB_EN						0x5490
+#define		FB_READ_EN					(1 << 0)
+#define		FB_WRITE_EN					(1 << 1)
+
 #define CP_ME_CNTL					0x86D8
 #define		CP_ME_HALT					(1 << 28)
 #define		CP_PFP_HALT					(1 << 26)
@@ -193,6 +197,9 @@
 #define		NOOFCHAN_SHIFT					12
 #define		NOOFCHAN_MASK					0x00003000
 #define MC_SHARED_CHREMAP					0x2008
+
+#define MC_SHARED_BLACKOUT_CNTL           		0x20ac
+#define		BLACKOUT_MODE_MASK			0x00000007
 
 #define	MC_ARB_RAMCFG					0x2760
 #define		NOOFBANK_SHIFT					0
