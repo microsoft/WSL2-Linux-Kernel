2a384d099ad3a3a89d8b98b6a90272a800729c46
Jerome Glisse <jglisse@redhat.com>
2012-12-14 16:20:46 -0500

    drm/radeon: restore modeset late in GPU reset path
    commit d3493574e267c203836bfdcb9c58d8af46fc0da1 upstream.
    
    Modeset path seems to conflict sometimes with the memory management
    leading to kernel deadlock. This move modesetting reset after GPU
    acceleration reset.
    
    Signed-off-by: Jerome Glisse <jglisse@redhat.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/radeon/radeon_device.c
+++ drivers/gpu/drm/radeon/radeon_device.c
@@ -1337,7 +1337,6 @@ retry:
 	}
 
 	radeon_restore_bios_scratch_regs(rdev);
-	drm_helper_resume_force_mode(rdev->ddev);
 
 	if (!r) {
 		for (i = 0; i < RADEON_NUM_RINGS; ++i) {
@@ -1361,6 +1360,8 @@ retry:
 			kfree(ring_data[i]);
 		}
 	}
+
+	drm_helper_resume_force_mode(rdev->ddev);
 
 	ttm_bo_unlock_delayed_workqueue(&rdev->mman.bdev, resched);
 	if (r) {
