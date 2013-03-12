3125bdd601790d03f36343d9d0941c5f94244f27
Eric W. Biederman <ebiederm@xmission.com>
2013-03-11 17:41:28 -0700

    decnet: Fix disappearing sysctl entries
    When decnet is built as a module a simple:
    echo 0.0 >/proc/sys/net/decnet/node_address
    
    results in most of the sysctl entries under /proc/sys/net/decnet and
    /proc/sys/net/decnet/conf disappearing.
    
    For more details see http://www.spinics.net/lists/netdev/msg226123.html.
    
    This change applies the same workaround used in
    net/core/sysctl_net_core.c and net/ipv6/sysctl_net_ipv6.c of creating
    a skeleton of decnet sysctl entries before doing anything else.
    
    The problem first appeared in kernel 2.6.27.  The later rewrite of
    sysctl in kernel 3.4 restored the previous behavior and eliminated the
    need for this workaround.
    
    This patch was heavily inspired by a similar but more complex patch by
    Larry Baker.
    
    Reported-by: Larry Baker <baker@usgs.gov>
    Signed-off-by: "Eric W. Biederman" <ebiederm@xmission.com>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- net/decnet/af_decnet.c
+++ net/decnet/af_decnet.c
@@ -2354,6 +2354,8 @@ static const struct proto_ops dn_proto_ops = {
 	.sendpage =	sock_no_sendpage,
 };
 
+void dn_register_sysctl_skeleton(void);
+void dn_unregister_sysctl_skeleton(void);
 void dn_register_sysctl(void);
 void dn_unregister_sysctl(void);
 
@@ -2374,6 +2376,7 @@ static int __init decnet_init(void)
 	if (rc != 0)
 		goto out;
 
+	dn_register_sysctl_skeleton();
 	dn_neigh_init();
 	dn_dev_init();
 	dn_route_init();
@@ -2413,6 +2416,7 @@ static void __exit decnet_exit(void)
 	dn_fib_cleanup();
 
 	proc_net_remove(&init_net, "decnet");
+	dn_unregister_sysctl_skeleton();
 
 	proto_unregister(&dn_proto);
 
