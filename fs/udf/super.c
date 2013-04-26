54c0b8dd3b71ae9d23ce78b7606e233b00e08b0e
George G. Davis <george_davis@mentor.com>
2013-04-26 19:00:53 -0400

    udf: fix udf_error build warnings
    The v2.6.34.14 commit "d7542a6 udf: Avoid run away loop when partition
    table length is corrupted" introduced the following build warning due
    to a change in the number of args in udf_error/udf_err for v2.6.34.14:
    
     CC      fs/udf/super.o
    fs/udf/super.c: In function 'udf_load_sparable_map':
    fs/udf/super.c:1259: warning: passing argument 3 of 'udf_error' makes pointer from integer without a cast
    fs/udf/super.c:1265: warning: passing argument 3 of 'udf_error' makes pointer from integer without a cast
    fs/udf/super.c: In function 'udf_load_logicalvol':
    fs/udf/super.c:1313: warning: passing argument 3 of 'udf_error' makes pointer from integer without a cast
    
    The above warnings are due to a missing __func__ argument in the above
    udf_error function calls.  This is because of commit 8076c363da15e7
    ("udf: Rename udf_error to udf_err") which removed the __func__ arg.
    
    Restore the missing __func__ argument to fix the build warnings.
    
    Signed-off-by: George G. Davis <george_davis@mentor.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- fs/udf/super.c
+++ fs/udf/super.c
@@ -1254,13 +1254,15 @@ static int udf_load_sparable_map(struct super_block *sb,
 	map->s_partition_type = UDF_SPARABLE_MAP15;
 	sdata->s_packet_len = le16_to_cpu(spm->packetLength);
 	if (!is_power_of_2(sdata->s_packet_len)) {
-		udf_error(sb, "error loading logical volume descriptor: "
+		udf_error(sb, __func__,
+			  "error loading logical volume descriptor: "
 			  "Invalid packet length %u\n",
 			  (unsigned)sdata->s_packet_len);
 		return -EIO;
 	}
 	if (spm->numSparingTables > 4) {
-		udf_error(sb, "error loading logical volume descriptor: "
+		udf_error(sb, __func__,
+			  "error loading logical volume descriptor: "
 			  "Too many sparing tables (%d)\n",
 			  (int)spm->numSparingTables);
 		return -EIO;
@@ -1308,7 +1310,8 @@ static int udf_load_logicalvol(struct super_block *sb, sector_t block,
 	lvd = (struct logicalVolDesc *)bh->b_data;
 	table_len = le32_to_cpu(lvd->mapTableLength);
 	if (table_len > sb->s_blocksize - sizeof(*lvd)) {
-		udf_error(sb, "error loading logical volume descriptor: "
+		udf_error(sb, __func__,
+			  "error loading logical volume descriptor: "
 			  "Partition table too long (%u > %lu)\n", table_len,
 			  sb->s_blocksize - sizeof(*lvd));
 		goto out_bh;
