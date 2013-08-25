0140c2fcecbdc5fdf70a10060147df9d8201d79e
Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
2013-08-24 23:38:15 -0400

    mmc: tmio_mmc_dma: fix PIO fallback on SDHI
    commit f936f9b67b7f8c2eae01dd303a0e90bd777c4679 upstream.
    
    I'm testing SH-Mobile SDHI driver in DMA mode with  a new DMA controller  using
    'bonnie++' and getting DMA error after which the tmio_mmc_dma.c code falls back
    to PIO but all commands time out after that.  It turned out that the fallback
    code calls tmio_mmc_enable_dma() with RX/TX channels already freed and pointers
    to them cleared, so that the function bails out early instead  of clearing the
    DMA bit in the CTL_DMA_ENABLE register. The regression was introduced by commit
    162f43e31c5a376ec16336e5d0ac973373d54c89 (mmc: tmio: fix a deadlock).
    Moving tmio_mmc_enable_dma() calls to the top of the PIO fallback code in
    tmio_mmc_start_dma_{rx|tx}() helps.
    
    Signed-off-by: Sergei Shtylyov <sergei.shtylyov@cogentembedded.com>
    Acked-by: Guennadi Liakhovetski <g.liakhovetski@gmx.de>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/mmc/host/tmio_mmc_dma.c
+++ drivers/mmc/host/tmio_mmc_dma.c
@@ -92,6 +92,7 @@ static void tmio_mmc_start_dma_rx(struct tmio_mmc_host *host)
 pio:
 	if (!desc) {
 		/* DMA failed, fall back to PIO */
+		tmio_mmc_enable_dma(host, false);
 		if (ret >= 0)
 			ret = -EIO;
 		host->chan_rx = NULL;
@@ -104,7 +105,6 @@ pio:
 		}
 		dev_warn(&host->pdev->dev,
 			 "DMA failed: %d, falling back to PIO\n", ret);
-		tmio_mmc_enable_dma(host, false);
 	}
 
 	dev_dbg(&host->pdev->dev, "%s(): desc %p, cookie %d, sg[%d]\n", __func__,
@@ -173,6 +173,7 @@ static void tmio_mmc_start_dma_tx(struct tmio_mmc_host *host)
 pio:
 	if (!desc) {
 		/* DMA failed, fall back to PIO */
+		tmio_mmc_enable_dma(host, false);
 		if (ret >= 0)
 			ret = -EIO;
 		host->chan_tx = NULL;
@@ -185,7 +186,6 @@ pio:
 		}
 		dev_warn(&host->pdev->dev,
 			 "DMA failed: %d, falling back to PIO\n", ret);
-		tmio_mmc_enable_dma(host, false);
 	}
 
 	dev_dbg(&host->pdev->dev, "%s(): desc %p, cookie %d\n", __func__,
