dc12cddcebe3ba42e9d8c56b4605c440f3e4122f
Florian Westphal <fw@strlen.de>
2014-10-20 13:49:17 +0200

    net: make skb_gso_segment error handling more robust
    commit 330966e501ffe282d7184fde4518d5e0c24bc7f8 upstream.
    
    skb_gso_segment has three possible return values:
    1. a pointer to the first segmented skb
    2. an errno value (IS_ERR())
    3. NULL.  This can happen when GSO is used for header verification.
    
    However, several callers currently test IS_ERR instead of IS_ERR_OR_NULL
    and would oops when NULL is returned.
    
    Note that these call sites should never actually see such a NULL return
    value; all callers mask out the GSO bits in the feature argument.
    
    However, there have been issues with some protocol handlers erronously not
    respecting the specified feature mask in some cases.
    
    It is preferable to get 'have to turn off hw offloading, else slow' reports
    rather than 'kernel crashes'.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [Brad Spengler: backported to 3.2]
    Signed-off-by: Brad Spengler <spender@grsecurity.net>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- net/netfilter/nf_queue.c
+++ net/netfilter/nf_queue.c
@@ -233,7 +233,7 @@ int nf_queue(struct sk_buff *skb,
 	 * returned by nf_queue.  For instance, callers rely on -ECANCELED to mean
 	 * 'ignore this hook'.
 	 */
-	if (IS_ERR(segs))
+	if (IS_ERR_OR_NULL(segs))
 		return -EINVAL;
 
 	queued = 0;
