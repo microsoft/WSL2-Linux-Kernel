b53e4feee45c37422ca4aa4b3cff85520aec011e
Daniel Vetter <daniel.vetter@ffwll.ch>
2014-11-14 09:25:29 +0100

    drm/i915: drop WaSetupGtModeTdRowDispatch:snb
    commit 2208d655a91f9879bd9a39ff9df05dd668b3512c upstream.
    
    This reverts the regressing
    
    commit 6547fbdbfff62c99e4f7b4f985ff8b3454f33b0f
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Fri Dec 14 23:38:29 2012 +0100
    
        drm/i915: Implement WaSetupGtModeTdRowDispatch
    
    that causes GPU hangs immediately on boot.
    
    Reported-by: Leo Wolf <jclw@ymail.com>
    Bugzilla: https://bugs.freedesktop.org/show_bug.cgi?id=79996
    Cc: stable@vger.kernel.org (v3.8+)
    Signed-off-by: Daniel Vetter <daniel.vetter@intel.com>
    [Jani: amended the commit message slightly.]
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- drivers/gpu/drm/i915/intel_pm.c
+++ drivers/gpu/drm/i915/intel_pm.c
@@ -4823,11 +4823,6 @@ static void gen6_init_clock_gating(struct drm_device *dev)
 	I915_WRITE(_3D_CHICKEN,
 		   _MASKED_BIT_ENABLE(_3D_CHICKEN_HIZ_PLANE_DISABLE_MSAA_4X_SNB));
 
-	/* WaSetupGtModeTdRowDispatch:snb */
-	if (IS_SNB_GT1(dev))
-		I915_WRITE(GEN6_GT_MODE,
-			   _MASKED_BIT_ENABLE(GEN6_TD_FOUR_ROW_DISPATCH_DISABLE));
-
 	I915_WRITE(WM3_LP_ILK, 0);
 	I915_WRITE(WM2_LP_ILK, 0);
 	I915_WRITE(WM1_LP_ILK, 0);
