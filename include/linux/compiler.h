ce2584379f31cfc44bbe770bb78430950a24156e
Ben Hutchings <ben@decadent.org.uk>
2014-12-11 02:43:02 +0000

    compiler: Define OPTIMIZER_HIDE_VAR
    Part of upstream commit fe8c8a126806 ('crypto: more robust
    crypto_memneq'), needed by commit d4c5efdb9777 ('random: add and use
    memzero_explicit() for clearing data').

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -164,6 +164,10 @@ void ftrace_likely_update(struct ftrace_branch_data *f, int val, int expect);
     (typeof(ptr)) (__ptr + (off)); })
 #endif
 
+#ifndef OPTIMIZER_HIDE_VAR
+#define OPTIMIZER_HIDE_VAR(var) barrier()
+#endif
+
 #endif /* __KERNEL__ */
 
 #endif /* __ASSEMBLY__ */
