1a2ce896855bae80c079ce45d102633546a6a87f
Wang Nan <wangnan0@huawei.com>
2015-02-26 13:49:39 +0800

    x86/traps: Enable DEBUG_STACK after cpu_init() for TRAP_DB/BP
    commit b4d8327024637cb2a1f7910dcb5d0ad7a096f473 upstream.
    
    Before this patch early_trap_init() installs DEBUG_STACK for
    X86_TRAP_BP and X86_TRAP_DB. However, DEBUG_STACK doesn't work
    correctly until cpu_init() <-- trap_init().
    
    This patch passes 0 to set_intr_gate_ist() and
    set_system_intr_gate_ist() instead of DEBUG_STACK to let it use
    same stack as kernel, and installs DEBUG_STACK for them in
    trap_init().
    
    As core runs at ring 0 between early_trap_init() and
    trap_init(), there is no chance to get a bad stack before
    trap_init().
    
    As NMI is also enabled in trap_init(), we don't need to care
    about is_debug_stack() and related things used in
    arch/x86/kernel/nmi.c.
    
    Signed-off-by: Wang Nan <wangnan0@huawei.com>
    Reviewed-by: Masami Hiramatsu <masami.hiramatsu.pt@hitachi.com>
    Acked-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: <dave.hansen@linux.intel.com>
    Cc: <lizefan@huawei.com>
    Cc: <luto@amacapital.net>
    Cc: <oleg@redhat.com>
    Link: http://lkml.kernel.org/r/1424929779-13174-1-git-send-email-wangnan0@huawei.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [bwh: Backported to 3.2: adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- arch/x86/kernel/traps.c
+++ arch/x86/kernel/traps.c
@@ -718,9 +718,17 @@ dotraplinkage void do_iret_error(struct pt_regs *regs, long error_code)
 /* Set of traps needed for early debugging. */
 void __init early_trap_init(void)
 {
-	set_intr_gate_ist(X86_TRAP_DB, &debug, DEBUG_STACK);
+	/*
+	 * Don't set ist to DEBUG_STACK as it doesn't work until TSS is
+	 * ready in cpu_init() <-- trap_init(). Before trap_init(), CPU
+	 * runs at ring 0 so it is impossible to hit an invalid stack.
+	 * Using the original stack works well enough at this early
+	 * stage. DEBUG_STACK will be equipped after cpu_init() in
+	 * trap_init().
+	 */
+	set_intr_gate_ist(X86_TRAP_DB, &debug, 0);
 	/* int3 can be called from all */
-	set_system_intr_gate_ist(X86_TRAP_BP, &int3, DEBUG_STACK);
+	set_system_intr_gate_ist(X86_TRAP_BP, &int3, 0);
 	set_intr_gate(X86_TRAP_PF, &page_fault);
 	load_idt(&idt_descr);
 }
@@ -780,6 +788,15 @@ void __init trap_init(void)
 	 * Should be a barrier for any external CPU state:
 	 */
 	cpu_init();
+
+	/*
+	 * X86_TRAP_DB and X86_TRAP_BP have been set
+	 * in early_trap_init(). However, DEBUG_STACK works only after
+	 * cpu_init() loads TSS. See comments in early_trap_init().
+	 */
+	set_intr_gate_ist(X86_TRAP_DB, &debug, DEBUG_STACK);
+	/* int3 can be called from all */
+	set_system_intr_gate_ist(X86_TRAP_BP, &int3, DEBUG_STACK);
 
 	x86_init.irqs.trap_init();
 }
