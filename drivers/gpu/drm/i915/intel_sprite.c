f2d95aebb650ca307749c06d8cf97a1ad358d13d
Ville Syrjälä <ville.syrjala@linux.intel.com>
2015-03-27 19:59:40 +0200

    drm/i915: Reject the colorkey ioctls for primary and cursor planes
    commit 840a1cf0cd533f30da792527ca5ff6a023d4a727 upstream.
    
    The legcy colorkey ioctls are only implemented for sprite planes, so
    reject the ioctl for primary/cursor planes. If we want to support
    colorkeying with these planes (assuming we have hw support of course)
    we should just move ahead with the colorkey property conversion.
    
    Testcase: kms_legacy_colorkey
    Cc: Tommi Rantala <tt.rantala@gmail.com>
    Reference: http://mid.gmane.org/CA+ydwtr+bCo7LJ44JFmUkVRx144UDFgOS+aJTfK6KHtvBDVuAw@mail.gmail.com
    Reported-and-tested-by: Tommi Rantala <tt.rantala@gmail.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/gpu/drm/i915/intel_sprite.c
+++ drivers/gpu/drm/i915/intel_sprite.c
@@ -1495,7 +1495,7 @@ int intel_sprite_set_colorkey(struct drm_device *dev, void *data,
 	drm_modeset_lock_all(dev);
 
 	plane = drm_plane_find(dev, set->plane_id);
-	if (!plane) {
+	if (!plane || plane->type != DRM_PLANE_TYPE_OVERLAY) {
 		ret = -ENOENT;
 		goto out_unlock;
 	}
@@ -1522,7 +1522,7 @@ int intel_sprite_get_colorkey(struct drm_device *dev, void *data,
 	drm_modeset_lock_all(dev);
 
 	plane = drm_plane_find(dev, get->plane_id);
-	if (!plane) {
+	if (!plane || plane->type != DRM_PLANE_TYPE_OVERLAY) {
 		ret = -ENOENT;
 		goto out_unlock;
 	}
