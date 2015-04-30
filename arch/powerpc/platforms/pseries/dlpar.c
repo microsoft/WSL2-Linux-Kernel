c79a54264db288e0fc221a4f39981feea15447ab
Nathan Fontenot <nfont@linux.vnet.ibm.com>
2015-04-29 20:42:06 -0500

    powerpc/pseries: Correct cpu affinity for dlpar added cpus
    commit f32393c943e297b8ae180c8f83d81a156c7d0412 upstream.
    
    The incorrect ordering of operations during cpu dlpar add results in invalid
    affinity for the cpu being added. The ibm,associativity property in the
    device tree is populated with all zeroes for the added cpu which results in
    invalid affinity mappings and all cpus appear to belong to node 0.
    
    This occurs because rtas configure-connector is called prior to making the
    rtas set-indicator calls. Phyp does not assign affinity information
    for a cpu until the rtas set-indicator calls are made to set the isolation
    and allocation state.
    
    Correct the order of operations to make the rtas set-indicator
    calls (done in dlpar_acquire_drc) before calling rtas configure-connector.
    
    Fixes: 1a8061c46c46 ("powerpc/pseries: Add kernel based CPU DLPAR handling")
    
    Signed-off-by: Nathan Fontenot <nfont@linux.vnet.ibm.com>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    [lizf: Backported to 3.4:
     - adjust context
     - jump to the "out" lable instead of returning -EINVAL]
    Signed-off-by: Zefan Li <lizefan@huawei.com>

--- arch/powerpc/platforms/pseries/dlpar.c
+++ arch/powerpc/platforms/pseries/dlpar.c
@@ -416,6 +416,12 @@ static ssize_t dlpar_cpu_probe(const char *buf, size_t count)
 		goto out;
 	}
 
+	rc = dlpar_acquire_drc(drc_index);
+	if (rc) {
+		rc = -EINVAL;
+		goto out;
+	}
+
 	dn = dlpar_configure_connector(drc_index);
 	if (!dn) {
 		rc = -EINVAL;
@@ -435,13 +441,6 @@ static ssize_t dlpar_cpu_probe(const char *buf, size_t count)
 
 	kfree(dn->full_name);
 	dn->full_name = cpu_name;
-
-	rc = dlpar_acquire_drc(drc_index);
-	if (rc) {
-		dlpar_free_cc_nodes(dn);
-		rc = -EINVAL;
-		goto out;
-	}
 
 	rc = dlpar_attach_node(dn);
 	if (rc) {
