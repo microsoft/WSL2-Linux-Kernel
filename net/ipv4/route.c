f41c3e6ddce76b958ce2535fed90fd7a3489c4fd
Masatake YAMATO <yamato@redhat.com>
2015-07-09 12:46:35 +0900

    route: remove unsed variable in __mkroute_input
    commit cb1c61680d29a054b91a23c7a504cea8a72bdcff upstream.
    
    flags local variable in __mkroute_input is not used as a variable.
    
    Signed-off-by: Masatake YAMATO <yamato@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1587,7 +1587,6 @@ static int __mkroute_input(struct sk_buff *skb,
 	struct rtable *rth;
 	int err;
 	struct in_device *out_dev;
-	unsigned int flags = 0;
 	bool do_cache;
 	u32 itag = 0;
 
@@ -1660,7 +1659,7 @@ rt_cache:
 	}
 
 	rth->rt_genid = rt_genid_ipv4(dev_net(rth->dst.dev));
-	rth->rt_flags = flags;
+	rth->rt_flags = 0;
 	rth->rt_type = res->type;
 	rth->rt_is_input = 1;
 	rth->rt_iif 	= 0;
