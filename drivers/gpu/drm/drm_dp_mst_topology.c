e5c6bbbcc7be9106e744c36b88d0464a8a3c2a16
Dave Airlie <airlied@redhat.com>
2015-10-01 16:28:25 +1000

    drm/dp/mst: add some defines for logical/physical ports
    [ Upstream commit ccf03d6995fa4b784f5b987726ba98f4859bf326 ]
    
    This just removes the magic number.
    
    Acked-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>

--- drivers/gpu/drm/drm_dp_mst_topology.c
+++ drivers/gpu/drm/drm_dp_mst_topology.c
@@ -1156,7 +1156,7 @@ static void drm_dp_add_port(struct drm_dp_mst_branch *mstb,
 			drm_dp_put_port(port);
 			goto out;
 		}
-		if (port->port_num >= 8) {
+		if (port->port_num >= DP_MST_LOGICAL_PORT_0) {
 			port->cached_edid = drm_get_edid(port->connector, &port->aux.ddc);
 		}
 	}
