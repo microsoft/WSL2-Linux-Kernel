0ca709ad358a1cb9be5f594a6eca77cbb3b7d7a1
Zefan Li <lizefan@huawei.com>
2015-10-11 18:45:19 +0800

    ARM: Fix incorrect backport of 0b59d8806a31
    Reported-by: Jim Faulkner <jfaulkne@ccs.neu.edu>
    Fixed-by: Nicolas Schichan <nschichan@freebox.fr>
    Signed-off-by: Zefan Li <lizefan@huawei.com>

--- arch/arm/net/bpf_jit_32.c
+++ arch/arm/net/bpf_jit_32.c
@@ -899,7 +899,7 @@ void bpf_jit_compile(struct sk_filter *fp)
 		if (ctx.imm_count)
 			kfree(ctx.imms);
 #endif
-		bpf_jit_binary_free(header);
+		module_free(NULL, ctx.target);
 		goto out;
 	}
 	build_epilogue(&ctx);
