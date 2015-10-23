4561e822a9fb50555d1e2affa7e1d24a5db86090
Alex Deucher <alexander.deucher@amd.com>
2015-10-23 10:38:52 -0400

    drm/radeon: don't try to recreate sysfs entries on resume
    commit 49abb26651167c892393cd9f2ad23df429645ed9 upstream.
    
    Fixes a harmless error message caused by:
    51a4726b04e880fdd9b4e0e58b13f70b0a68a7f5
    
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- drivers/gpu/drm/radeon/radeon.h
+++ drivers/gpu/drm/radeon/radeon.h
@@ -1596,6 +1596,7 @@ struct radeon_pm {
 	struct device	        *int_hwmon_dev;
 	/* dpm */
 	bool                    dpm_enabled;
+	bool                    sysfs_initialized;
 	struct radeon_dpm       dpm;
 };
 
