d4b9251744b47d562363bfa7ff005b938f705c8c
Vasu Dev <vasu.dev@intel.com>
2015-11-23 10:31:01 -0800

    ixgbe: use correct FCoE DDP max check
    commit f10166aba2def9bc6443290231c60f7e2f70129b upstream.
    
    Use fcoe_ddp_xid from netdev as this is correctly set for different
    device IDs to avoid DDP skip error on X550 as "xid=0x20b out-of-range"
    
    Signed-off-by: Vasu Dev <vasu.dev@intel.com>
    Tested-by: Phil Schmitt <phillip.j.schmitt@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_fcoe.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_fcoe.c
@@ -75,7 +75,7 @@ int ixgbe_fcoe_ddp_put(struct net_device *netdev, u16 xid)
 	if (!netdev)
 		goto out_ddp_put;
 
-	if (xid >= IXGBE_FCOE_DDP_MAX)
+	if (xid >= netdev->fcoe_ddp_xid)
 		goto out_ddp_put;
 
 	adapter = netdev_priv(netdev);
@@ -150,7 +150,7 @@ static int ixgbe_fcoe_ddp_setup(struct net_device *netdev, u16 xid,
 		return 0;
 
 	adapter = netdev_priv(netdev);
-	if (xid >= IXGBE_FCOE_DDP_MAX) {
+	if (xid >= netdev->fcoe_ddp_xid) {
 		e_warn(drv, "xid=0x%x out-of-range\n", xid);
 		return 0;
 	}
