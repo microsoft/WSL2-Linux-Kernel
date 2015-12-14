35da6d62b608fd3beb576d72e48e2bc866d714c9
Hannes Frederic Sowa <hannes@stressinduktion.org>
2015-12-14 23:30:43 +0100

    net: fix warnings in 'make htmldocs' by moving macro definition out of field declaration
    commit 7bbadd2d1009575dad675afc16650ebb5aa10612 upstream.
    
    Docbook does not like the definition of macros inside a field declaration
    and adds a warning. Move the definition out.
    
    Fixes: 79462ad02e86180 ("net: add validation for the socket syscall protocol argument")
    Reported-by: kbuild test robot <lkp@intel.com>
    Signed-off-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [bwh: Backported to 3.2: keep open-coding U8_MAX]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- include/net/sock.h
+++ include/net/sock.h
@@ -302,8 +302,8 @@ struct sock {
 				sk_no_check  : 2,
 				sk_userlocks : 4,
 				sk_protocol  : 8,
-#define SK_PROTOCOL_MAX ((u8)~0U)
 				sk_type      : 16;
+#define SK_PROTOCOL_MAX ((u8)~0U)
 	kmemcheck_bitfield_end(flags);
 	int			sk_wmem_queued;
 	gfp_t			sk_allocation;
