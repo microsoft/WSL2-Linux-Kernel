86fa80c5587549ed7bfb8b807307269304d23e00
Ben Skeggs <bskeggs@redhat.com>
2016-01-08 08:56:51 +1000

    drm/nouveau/kms: take mode_config mutex in connector hotplug path
    commit 0a882cadbc63fd2da3994af7115b4ada2fcbd638 upstream.
    
    fdo#93634
    
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Signed-off-by: Luis Henriques <luis.henriques@canonical.com>

--- drivers/gpu/drm/nouveau/nouveau_connector.c
+++ drivers/gpu/drm/nouveau/nouveau_connector.c
@@ -954,10 +954,13 @@ nouveau_connector_hotplug_work(struct work_struct *work)
 
 		NV_DEBUG(drm, "%splugged %s\n", plugged ? "" : "un", name);
 
+		mutex_lock(&drm->dev->mode_config.mutex);
 		if (plugged)
 			drm_helper_connector_dpms(connector, DRM_MODE_DPMS_ON);
 		else
 			drm_helper_connector_dpms(connector, DRM_MODE_DPMS_OFF);
+		mutex_unlock(&drm->dev->mode_config.mutex);
+
 		drm_helper_hpd_irq_event(connector->dev);
 	}
 
