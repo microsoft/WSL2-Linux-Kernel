eeecc3f39587463b95ea8ddd7b7ec2c2155e0782
Insu Yun <wuninsu@gmail.com>
2016-01-23 15:44:19 -0500

    ACPI / PCI / hotplug: unlock in error path in acpiphp_enable_slot()
    commit 2c3033a0664dfae91e1dee7fabac10f24354b958 upstream.
    
    In acpiphp_enable_slot(), there is a missing unlock path
    when error occurred.  It needs to be unlocked before returning
    an error.
    
    Signed-off-by: Insu Yun <wuninsu@gmail.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/pci/hotplug/acpiphp_glue.c
+++ drivers/pci/hotplug/acpiphp_glue.c
@@ -1133,8 +1133,10 @@ int acpiphp_enable_slot(struct acpiphp_slot *slot)
 {
 	pci_lock_rescan_remove();
 
-	if (slot->flags & SLOT_IS_GOING_AWAY)
+	if (slot->flags & SLOT_IS_GOING_AWAY) {
+		pci_unlock_rescan_remove();
 		return -ENODEV;
+	}
 
 	mutex_lock(&slot->crit_sect);
 	/* configure all functions */
