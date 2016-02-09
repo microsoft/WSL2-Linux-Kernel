77bff52c2c82d3b6c3ecb8f33b104f29a3edf732
Daniel Wagner <daniel.wagner@bmw-carit.de>
2016-02-09 10:18:32 +0100

    video: Use bool instead int pointer for get_opt_bool() argument
    [ Upstream commit 206fc20598157ce15597822cf01b94377e30075b ]
    
    As the function name already indicates that get_opt_bool() parses
    for a bool. It is not a surprise that compiler is complaining
    about it when -Werror=incompatible-pointer-types is used:
    
    drivers/video/fbdev/intelfb/intelfbdrv.c: In function ‘intelfb_setup’:
    drivers/video/fbdev/intelfb/intelfbdrv.c:353:39: error: passing argument 3 of ‘get_opt_bool’ from incompatible pointer type [-Werror=incompatible-pointer-types]
       if (get_opt_bool(this_opt, "accel", &accel))
    
    Signed-off-by: Daniel Wagner <daniel.wagner@bmw-carit.de>
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>

--- drivers/video/fbdev/intelfb/intelfbdrv.c
+++ drivers/video/fbdev/intelfb/intelfbdrv.c
@@ -306,7 +306,7 @@ static __inline__ int get_opt_int(const char *this_opt, const char *name,
 }
 
 static __inline__ int get_opt_bool(const char *this_opt, const char *name,
-				   int *ret)
+				   bool *ret)
 {
 	if (!ret)
 		return 0;
