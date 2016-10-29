6ca68d9535acf388b07d141a743316632d8b3cbe
Liping Zhang <zlpnobody@gmail.com>
2016-10-29 22:03:05 +0800

    netfilter: nf_tables: destroy the set if fail to add transaction
    commit c17c3cdff10b9f59ef1244a14604f10949f17117 upstream.
    
    When the memory is exhausted, then we will fail to add the NFT_MSG_NEWSET
    transaction. In such case, we should destroy the set before we free it.
    
    Fixes: 958bee14d071 ("netfilter: nf_tables: use new transaction infrastructure to handle sets")
    Signed-off-by: Liping Zhang <zlpnobody@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -2642,12 +2642,14 @@ static int nf_tables_newset(struct sock *nlsk, struct sk_buff *skb,
 
 	err = nft_trans_set_add(&ctx, NFT_MSG_NEWSET, set);
 	if (err < 0)
-		goto err2;
+		goto err3;
 
 	list_add_tail_rcu(&set->list, &table->sets);
 	table->use++;
 	return 0;
 
+err3:
+	ops->destroy(set);
 err2:
 	kfree(set);
 err1:
