c7a08d877c2095e536f95f8e915d074fd38637fe
Ngai-Mint Kwan <ngai-mint.kwan@intel.com>
2016-11-02 16:44:46 -0700

    fm10k: request reset when mbx->state changes
    [ Upstream commit 2f3fc1e6200309ccf87f61dea56e57e563c4f800 ]
    
    Multiple IES API resets can cause a race condition where the mailbox
    interrupt request bits can be cleared before being handled. This can
    leave certain mailbox messages from the PF to be untreated and the PF
    will enter in some inactive state. If this situation occurs, the IES API
    will initiate a mailbox version reset which, then, trigger a mailbox
    state change. Once this mailbox transition occurs (from OPEN to CONNECT
    state), a request for reset will be returned.
    
    This ensures that PF will undergo a reset whenever IES API encounters an
    unknown global mailbox interrupt event or whenever the IES API
    terminates.
    
    Signed-off-by: Ngai-Mint Kwan <ngai-mint.kwan@intel.com>
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Tested-by: Krishneil Singh <krishneil.k.singh@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>

--- drivers/net/ethernet/intel/fm10k/fm10k_mbx.c
+++ drivers/net/ethernet/intel/fm10k/fm10k_mbx.c
@@ -1967,9 +1967,10 @@ static void fm10k_sm_mbx_create_reply(struct fm10k_hw *hw,
  *  function can also be used to respond to an error as the connection
  *  resetting would also be a means of dealing with errors.
  **/
-static void fm10k_sm_mbx_process_reset(struct fm10k_hw *hw,
-				       struct fm10k_mbx_info *mbx)
+static s32 fm10k_sm_mbx_process_reset(struct fm10k_hw *hw,
+				      struct fm10k_mbx_info *mbx)
 {
+	s32 err = 0;
 	const enum fm10k_mbx_state state = mbx->state;
 
 	switch (state) {
@@ -1982,6 +1983,7 @@ static void fm10k_sm_mbx_process_reset(struct fm10k_hw *hw,
 	case FM10K_STATE_OPEN:
 		/* flush any incomplete work */
 		fm10k_sm_mbx_connect_reset(mbx);
+		err = FM10K_ERR_RESET_REQUESTED;
 		break;
 	case FM10K_STATE_CONNECT:
 		/* Update remote value to match local value */
@@ -1991,6 +1993,8 @@ static void fm10k_sm_mbx_process_reset(struct fm10k_hw *hw,
 	}
 
 	fm10k_sm_mbx_create_reply(hw, mbx, mbx->tail);
+
+	return err;
 }
 
 /**
@@ -2071,7 +2075,7 @@ static s32 fm10k_sm_mbx_process(struct fm10k_hw *hw,
 
 	switch (FM10K_MSG_HDR_FIELD_GET(mbx->mbx_hdr, SM_VER)) {
 	case 0:
-		fm10k_sm_mbx_process_reset(hw, mbx);
+		err = fm10k_sm_mbx_process_reset(hw, mbx);
 		break;
 	case FM10K_SM_MBX_VERSION:
 		err = fm10k_sm_mbx_process_version_1(hw, mbx);
