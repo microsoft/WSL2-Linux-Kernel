74aff38ef71d07d5f37b52ce3e945edd4d9ca7d6
Lendacky, Thomas <Thomas.Lendacky@amd.com>
2017-01-20 12:14:13 -0600

    amd-xgbe: Check xgbe_init() return code
    [ Upstream commit 738f7f647371ff4cfc9646c99dba5b58ad142db3 ]
    
    The xgbe_init() routine returns a return code indicating success or
    failure, but the return code is not checked. Add code to xgbe_init()
    to issue a message when failures are seen and add code to check the
    xgbe_init() return code.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <alexander.levin@verizon.com>

--- drivers/net/ethernet/amd/xgbe/xgbe-drv.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-drv.c
@@ -951,7 +951,9 @@ static int xgbe_start(struct xgbe_prv_data *pdata)
 
 	DBGPR("-->xgbe_start\n");
 
-	hw_if->init(pdata);
+	ret = hw_if->init(pdata);
+	if (ret)
+		return ret;
 
 	phy_start(pdata->phydev);
 
