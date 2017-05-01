44fda124389ae73ebfbde33ea196cc0db7339b1d
David S. Miller <davem@davemloft.net>
2017-05-01 15:10:20 -0400

    ipv6: Need to export ipv6_push_frag_opts for tunneling now.
    commit 5b8481fa42ac58484d633b558579e302aead64c1 upstream.
    
    Since that change also made the nfrag function not necessary
    for exports, remove it.
    
    Fixes: 89a23c8b528b ("ip6_tunnel: Fix missing tunnel encapsulation limit option")
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [bwh: Backported to 3.16: keep exporting ipv6_push_nfrag_opts(), needed by
     ip6_gre module]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- net/ipv6/exthdrs.c
+++ net/ipv6/exthdrs.c
@@ -709,6 +709,7 @@ void ipv6_push_frag_opts(struct sk_buff *skb, struct ipv6_txoptions *opt, u8 *pr
 	if (opt->dst1opt)
 		ipv6_push_exthdr(skb, proto, NEXTHDR_DEST, opt->dst1opt);
 }
+EXPORT_SYMBOL(ipv6_push_frag_opts);
 
 struct ipv6_txoptions *
 ipv6_dup_options(struct sock *sk, struct ipv6_txoptions *opt)
