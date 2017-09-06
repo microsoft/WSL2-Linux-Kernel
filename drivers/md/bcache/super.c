94e7963c765f300f2ed7d7adb0f2bd266fd374ac
Dan Carpenter <dan.carpenter@oracle.com>
2017-09-06 14:26:00 +0800

    bcache: silence static checker warning
    commit da22f0eea555baf9b0a84b52afe56db2052cfe8d upstream.
    
    In olden times, closure_return() used to have a hidden return built in.
    We removed the hidden return but forgot to add a new return here.  If
    "c" were NULL we would oops on the next line, but fortunately "c" is
    never NULL.  Let's just remove the if statement.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Reviewed-by: Coly Li <colyli@suse.de>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/md/bcache/super.c
+++ drivers/md/bcache/super.c
@@ -1398,9 +1398,6 @@ static void cache_set_flush(struct closure *cl)
 	struct btree *b;
 	unsigned i;
 
-	if (!c)
-		closure_return(cl);
-
 	bch_cache_accounting_destroy(&c->accounting);
 
 	kobject_put(&c->internal);
