f045a2851f9fe5a719fd76061263e017c610a542
Kiernan Hager <kah.listaddress@gmail.com>
2017-11-20 14:18:44 -0700

    platform/x86: asus-nb-wmi: Support ALS on the Zenbook UX430UQ
    [ Upstream commit db2582afa7444a0ce6bb1ebf1431715969a10b06 ]
    
    This patch adds support for ALS on the Zenbook UX430UQ to the asus_nb_wmi
    driver. It also renames "quirk_asus_ux330uak" to "quirk_asus_forceals"
    because it is now used for more than one model of computer, and should
    thus have a more general name.
    
    Signed-off-by: Kiernan Hager <kah.listaddress@gmail.com>
    [andy: massaged commit message, fixed indentation and commas in the code]
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/platform/x86/asus-nb-wmi.c
+++ drivers/platform/x86/asus-nb-wmi.c
@@ -120,7 +120,7 @@ static struct quirk_entry quirk_asus_x550lb = {
 	.xusb2pr = 0x01D9,
 };
 
-static struct quirk_entry quirk_asus_ux330uak = {
+static struct quirk_entry quirk_asus_forceals = {
 	.wmi_force_als_set = true,
 };
 
@@ -422,7 +422,7 @@ static const struct dmi_system_id asus_quirks[] = {
 			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
 			DMI_MATCH(DMI_PRODUCT_NAME, "UX330UAK"),
 		},
-		.driver_data = &quirk_asus_ux330uak,
+		.driver_data = &quirk_asus_forceals,
 	},
 	{
 		.callback = dmi_matched,
@@ -432,6 +432,15 @@ static const struct dmi_system_id asus_quirks[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "X550LB"),
 		},
 		.driver_data = &quirk_asus_x550lb,
+	},
+	{
+		.callback = dmi_matched,
+		.ident = "ASUSTeK COMPUTER INC. UX430UQ",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "UX430UQ"),
+		},
+		.driver_data = &quirk_asus_forceals,
 	},
 	{},
 };
