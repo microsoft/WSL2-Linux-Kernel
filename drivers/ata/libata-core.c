68c46dedf9357b8f498771ca86682b914b68c272
François Cami <fcami@fedoraproject.org>
2018-05-13 20:11:15 +0200

    libata: Apply NOLPM quirk for SAMSUNG PM830 CXM13D1Q.
    commit 76936e9a6df17b89481bd2655c8684291afbe656 upstream.
    
    Without this patch the drive errors out regularly:
    
    [    1.090154] ata1.00: ATA-8: SAMSUNG SSD PM830 mSATA 256GB,
    CXM13D1Q, max UDMA/133
    (...)
    [  345.154996] ata1.00: exception Emask 0x40 SAct 0x0 SErr 0xc0800 action 0x6
    [  345.155006] ata1.00: irq_stat 0x40000001
    [  345.155013] ata1: SError: { HostInt CommWake 10B8B }
    [  345.155018] ata1.00: failed command: SET FEATURES
    [  345.155032] ata1.00: cmd ef/05:e1:00:00:00/00:00:00:00:00/40 tag 7
                            res 51/04:e1:00:00:00/00:00:00:00:00/40 Emask 0x41 (internal error)
    [  345.155038] ata1.00: status: { DRDY ERR }
    [  345.155042] ata1.00: error: { ABRT }
    [  345.155051] ata1: hard resetting link
    [  345.465661] ata1: SATA link up 6.0 Gbps (SStatus 133 SControl 300)
    [  345.466955] ata1.00: configured for UDMA/133
    [  345.467085] ata1: EH complete
    
    Signed-off-by: François Cami <fcami@fedoraproject.org>
    Acked-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -4246,8 +4246,9 @@ static const struct ata_blacklist_entry ata_device_blacklist [] = {
 	{ "Crucial_CT960M500*",		NULL,	ATA_HORKAGE_NO_NCQ_TRIM |
 						ATA_HORKAGE_NOLPM, },
 
-	/* This specific Samsung model/firmware-rev does not handle LPM well */
+	/* These specific Samsung models/firmware-revs do not handle LPM well */
 	{ "SAMSUNG MZMPC128HBFU-000MV", "CXM14M1Q", ATA_HORKAGE_NOLPM, },
+	{ "SAMSUNG SSD PM830 mSATA *",  "CXM13D1Q", ATA_HORKAGE_NOLPM, },
 
 	/* devices that don't properly handle queued TRIM commands */
 	{ "Micron_M500IT_*",		"MU01",	ATA_HORKAGE_NO_NCQ_TRIM, },
