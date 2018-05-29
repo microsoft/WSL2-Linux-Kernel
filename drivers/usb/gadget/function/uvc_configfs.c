bfdb08934b2c10f9d3a7a054067b49d54dc247d0
Joel Pepper <joel.pepper@rwth-aachen.de>
2018-05-29 21:02:12 +0200

    usb: gadget: uvc: configfs: Prevent format changes after linking header
    [ Upstream commit cb2200f7af8341aaf0c6abd7ba37e4c667c41639 ]
    
    While checks are in place to avoid attributes and children of a format
    being manipulated after the format is linked into the streaming header,
    the linked flag was never actually set, invalidating the protections.
    Update the flag as appropriate in the header link calls.
    
    Signed-off-by: Joel Pepper <joel.pepper@rwth-aachen.de>
    Reviewed-by: Kieran Bingham <kieran.bingham@ideasonboard.com>
    Signed-off-by: Laurent Pinchart <laurent.pinchart@ideasonboard.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/usb/gadget/function/uvc_configfs.c
+++ drivers/usb/gadget/function/uvc_configfs.c
@@ -772,6 +772,7 @@ static int uvcg_streaming_header_allow_link(struct config_item *src,
 	format_ptr->fmt = target_fmt;
 	list_add_tail(&format_ptr->entry, &src_hdr->formats);
 	++src_hdr->num_fmt;
+	++target_fmt->linked;
 
 out:
 	mutex_unlock(&opts->lock);
@@ -809,6 +810,8 @@ static int uvcg_streaming_header_drop_link(struct config_item *src,
 			--src_hdr->num_fmt;
 			break;
 		}
+
+	--target_fmt->linked;
 
 out:
 	mutex_unlock(&opts->lock);
