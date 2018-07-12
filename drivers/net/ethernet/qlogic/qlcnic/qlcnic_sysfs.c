02d0034a92e79cfd68c50922b302b04a6c10c2d0
Dan Carpenter <dan.carpenter@oracle.com>
2018-07-12 15:23:45 +0300

    qlogic: check kstrtoul() for errors
    commit 5fc853cc01c68f84984ecc2d5fd777ecad78240f upstream.
    
    We accidentally left out the error handling for kstrtoul().
    
    Fixes: a520030e326a ("qlcnic: Implement flash sysfs callback for 83xx adapter")
    Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- drivers/net/ethernet/qlogic/qlcnic/qlcnic_sysfs.c
+++ drivers/net/ethernet/qlogic/qlcnic/qlcnic_sysfs.c
@@ -1122,6 +1122,8 @@ static ssize_t qlcnic_83xx_sysfs_flash_write_handler(struct file *filp,
 		return QL_STATUS_INVALID_PARAM;
 
 	ret = kstrtoul(buf, 16, &data);
+	if (ret)
+		return ret;
 
 	switch (data) {
 	case QLC_83XX_FLASH_SECTOR_ERASE_CMD:
