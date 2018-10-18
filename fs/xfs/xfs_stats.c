fd608131a916d077e6abd3bab7b63f600bafd707
Carlos Maiolino <cmaiolino@redhat.com>
2018-10-18 17:21:34 +1100

    xfs: Fix xqmstats offsets in /proc/fs/xfs/xqmstat
    commit 41657e5507b13e963be906d5d874f4f02374fd5c upstream.
    
    The addition of FIBT, RMAP and REFCOUNT changed the offsets into
    __xfssats structure.
    
    This caused xqmstat_proc_show() to display garbage data via
    /proc/fs/xfs/xqmstat, once it relies on the offsets marked via macros.
    
    Fix it.
    
    Fixes: 00f4e4f9 xfs: add rmap btree stats infrastructure
    Fixes: aafc3c24 xfs: support the XFS_BTNUM_FINOBT free inode btree type
    Fixes: 46eeb521 xfs: introduce refcount btree definitions
    Signed-off-by: Carlos Maiolino <cmaiolino@redhat.com>
    Reviewed-by: Eric Sandeen <sandeen@redhat.com>
    Signed-off-by: Dave Chinner <david@fromorbit.com>
    [bwh: Backported to 3.16:
     - Only the FIBT stats have been added, so start from XFSSTAT_END_FIBT_V2
     - Adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- fs/xfs/xfs_stats.c
+++ fs/xfs/xfs_stats.c
@@ -135,7 +135,7 @@ static int xqmstat_proc_show(struct seq_file *m, void *v)
 	int j;
 
 	seq_printf(m, "qm");
-	for (j = XFSSTAT_END_IBT_V2; j < XFSSTAT_END_XQMSTAT; j++)
+	for (j = XFSSTAT_END_FIBT_V2; j < XFSSTAT_END_XQMSTAT; j++)
 		seq_printf(m, " %u", counter_val(j));
 	seq_putc(m, '\n');
 	return 0;
