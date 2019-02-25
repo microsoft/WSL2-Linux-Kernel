00d16bce40fa2528cd41a15f2ac93da2aa673c6f
Dave Hansen <dave.hansen@linux.intel.com>
2019-02-25 10:57:38 -0800

    mm/resource: Let walk_system_ram_range() search child resources
    In the process of onlining memory, we use walk_system_ram_range()
    to find the actual RAM areas inside of the area being onlined.
    
    However, it currently only finds memory resources which are
    "top-level" iomem_resources.  Children are not currently
    searched which causes it to skip System RAM in areas like this
    (in the format of /proc/iomem):
    
    a0000000-bfffffff : Persistent Memory (legacy)
      a0000000-afffffff : System RAM
    
    Changing the true->false here allows children to be searched
    as well.  We need this because we add a new "System RAM"
    resource underneath the "persistent memory" resource when
    we use persistent memory in a volatile mode.
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Keith Busch <keith.busch@intel.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Ross Zwisler <zwisler@kernel.org>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: linux-nvdimm@lists.01.org
    Cc: linux-kernel@vger.kernel.org
    Cc: linux-mm@kvack.org
    Cc: Huang Ying <ying.huang@intel.com>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Borislav Petkov <bp@suse.de>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Yaowei Bai <baiyaowei@cmss.chinamobile.com>
    Cc: Takashi Iwai <tiwai@suse.de>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    
    TAG_MSFT: overlake
    (cherry picked from commit 2b539aefe9e48e3908cff02699aa63a8b9bd268e)
    Signed-off-by: Pavel Tatashin <pasha.tatashin@soleen.com>

--- kernel/resource.c
+++ kernel/resource.c
@@ -460,6 +460,9 @@ int walk_mem_res(u64 start, u64 end, void *arg,
  * This function calls the @func callback against all memory ranges of type
  * System RAM which are marked as IORESOURCE_SYSTEM_RAM and IORESOUCE_BUSY.
  * It is to be used only for System RAM.
+ *
+ * This will find System RAM ranges that are children of top-level resources
+ * in addition to top-level System RAM resources.
  */
 int walk_system_ram_range(unsigned long start_pfn, unsigned long nr_pages,
 		void *arg, int (*func)(unsigned long, unsigned long, void *))
@@ -474,7 +477,7 @@ int walk_system_ram_range(unsigned long start_pfn, unsigned long nr_pages,
 	res.flags = IORESOURCE_SYSTEM_RAM | IORESOURCE_BUSY;
 	orig_end = res.end;
 	while ((res.start < res.end) &&
-		(find_next_iomem_res(&res, IORES_DESC_NONE, true) >= 0)) {
+		(find_next_iomem_res(&res, IORES_DESC_NONE, false) >= 0)) {
 		pfn = (res.start + PAGE_SIZE - 1) >> PAGE_SHIFT;
 		end_pfn = (res.end + 1) >> PAGE_SHIFT;
 		if (end_pfn > pfn)
