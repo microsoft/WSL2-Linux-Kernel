05f4b670242e2b27c95d6eca88ac1fbf79c27a56
Ben Hutchings <ben@decadent.org.uk>
2019-08-09 20:09:49 +0100

    Revert "inet: update the IP ID generation algorithm to higher standards."
    This reverts commit 8b197d3ce585d6777197e0633d71e5af7d98cb35, which
    was a stable-specific improvement to IP ID selection.  I will apply
    the upstream changes instead.
    
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -487,15 +487,13 @@ EXPORT_SYMBOL(ip_idents_reserve);
 void __ip_select_ident(struct iphdr *iph, int segs)
 {
 	static u32 ip_idents_hashrnd __read_mostly;
-	static u32 ip_idents_hashrnd_extra __read_mostly;
 	u32 hash, id;
 
 	net_get_random_once(&ip_idents_hashrnd, sizeof(ip_idents_hashrnd));
-	net_get_random_once(&ip_idents_hashrnd_extra, sizeof(ip_idents_hashrnd_extra));
 
 	hash = jhash_3words((__force u32)iph->daddr,
 			    (__force u32)iph->saddr,
-			    iph->protocol ^ ip_idents_hashrnd_extra,
+			    iph->protocol,
 			    ip_idents_hashrnd);
 	id = ip_idents_reserve(hash, segs);
 	iph->id = htons(id);
