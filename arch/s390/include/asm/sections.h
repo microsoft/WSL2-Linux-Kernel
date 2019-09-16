a591cb4988667d2d10228617f96fb63dbfbf2394
AS <atlestromsvold@hotmail.no>
2019-09-16 14:34:49 +0200

    com

--- arch/s390/include/asm/sections.h
+++
@@ -1,7 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _S390_SECTIONS_H
-#define _S390_SECTIONS_H
-
-#include <asm-generic/sections.h>
-
-#endif
--- tools/build/feature/test-reallocarray.c
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0
-#define _GNU_SOURCE
-#include <stdlib.h>
-
-int main(void)
-{
-	return !!reallocarray(NULL, 1, 1);
-}
