96cfb5023296450f33555a34121ea0ab7060cdae
Prakhar Srivastava <prsriva@linux.microsoft.com>
2019-10-10 17:36:00 -0700

    update powerpc implementation to call into of_ima*
    update powerpc ima buffer pass implementationt to call into
    of_ima* for a cross architecture support.
    
    Signed-off-by: Prakhar Srivastava <prsriva@linux.microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/powerpc/include/asm/ima.h
+++ arch/powerpc/include/asm/ima.h
@@ -6,12 +6,7 @@ struct kimage;
 
 int ima_get_kexec_buffer(void **addr, size_t *size);
 int ima_free_kexec_buffer(void);
-
-#ifdef CONFIG_IMA
 void remove_ima_buffer(void *fdt, int chosen_node);
-#else
-static inline void remove_ima_buffer(void *fdt, int chosen_node) {}
-#endif
 
 #ifdef CONFIG_IMA_KEXEC
 int arch_ima_add_kexec_buffer(struct kimage *image, unsigned long load_addr,
