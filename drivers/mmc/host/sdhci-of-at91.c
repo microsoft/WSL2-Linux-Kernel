b827e2fa580bbada46fff51d712748bb4aa92208
Eugen Hristev <eugen.hristev@microchip.com>
2019-11-14 12:59:26 +0000

    mmc: sdhci-of-at91: fix quirk2 overwrite
    commit fed23c5829ecab4ddc712d7b0046e59610ca3ba4 upstream.
    
    The quirks2 are parsed and set (e.g. from DT) before the quirk for broken
    HS200 is set in the driver.
    The driver needs to enable just this flag, not rewrite the whole quirk set.
    
    Fixes: 7871aa60ae00 ("mmc: sdhci-of-at91: add quirk for broken HS200")
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/mmc/host/sdhci-of-at91.c
+++ drivers/mmc/host/sdhci-of-at91.c
@@ -145,7 +145,7 @@ static int sdhci_at91_probe(struct platform_device *pdev)
 	sdhci_get_of_property(pdev);
 
 	/* HS200 is broken at this moment */
-	host->quirks2 = SDHCI_QUIRK2_BROKEN_HS200;
+	host->quirks2 |= SDHCI_QUIRK2_BROKEN_HS200;
 
 	ret = sdhci_add_host(host);
 	if (ret)
