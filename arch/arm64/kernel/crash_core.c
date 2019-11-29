16e3920a3cc89d4c3ddbfb79bb85524b95fe60ce
Bhupesh Sharma <bhsharma@redhat.com>
2019-11-30 01:29:16 +0530

    arm64/crash_core: Export TCR_EL1.T1SZ in vmcoreinfo
    vabits_actual variable on arm64 indicates the actual VA space size,
    and allows a single binary to support both 48-bit and 52-bit VA
    spaces.
    
    If the ARMv8.2-LVA optional feature is present, and we are running
    with a 64KB page size; then it is possible to use 52-bits of address
    space for both userspace and kernel addresses. However, any kernel
    binary that supports 52-bit must also be able to fall back to 48-bit
    at early boot time if the hardware feature is not present.
    
    Since TCR_EL1.T1SZ indicates the size offset of the memory region
    addressed by TTBR1_EL1 (and hence can be used for determining the
    vabits_actual value) it makes more sense to export the same in
    vmcoreinfo rather than vabits_actual variable, as the name of the
    variable can change in future kernel versions, but the architectural
    constructs like TCR_EL1.T1SZ can be used better to indicate intended
    specific fields to user-space.
    
    User-space utilities like makedumpfile and crash-utility, need to
    read/write this value from/to vmcoreinfo for determining if a virtual
    address lies in the linear map range.
    
    The user-space computation for determining whether an address lies in
    the linear map range is the same as we have in kernel-space:
    
      #define __is_lm_address(addr)	(!(((u64)addr) & BIT(vabits_actual - 1)))
    
    I have sent out user-space patches for makedumpfile and crash-utility
    to add features for obtaining vabits_actual value from TCR_EL1.T1SZ (see
    [0] and [1]).
    
    Akashi reported that he was able to use this patchset and the user-space
    changes to get user-space working fine with the 52-bit kernel VA
    changes (see [2]).
    
    [0]. http://lists.infradead.org/pipermail/kexec/2019-November/023966.html
    [1]. http://lists.infradead.org/pipermail/kexec/2019-November/024006.html
    [2]. http://lists.infradead.org/pipermail/kexec/2019-November/023992.html
    
    Cc: James Morse <james.morse@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Steve Capper <steve.capper@arm.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Cc: Dave Anderson <anderson@redhat.com>
    Cc: Kazuhito Hagio <k-hagio@ab.jp.nec.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Cc: kexec@lists.infradead.org
    Signed-off-by: Bhupesh Sharma <bhsharma@redhat.com>

--- arch/arm64/kernel/crash_core.c
+++ arch/arm64/kernel/crash_core.c
@@ -7,6 +7,13 @@
 #include <linux/crash_core.h>
 #include <asm/memory.h>
 
+static inline u64 get_tcr_el1_t1sz(void);
+
+static inline u64 get_tcr_el1_t1sz(void)
+{
+	return (read_sysreg(tcr_el1) & TCR_T1SZ_MASK) >> TCR_T1SZ_OFFSET;
+}
+
 void arch_crash_save_vmcoreinfo(void)
 {
 	VMCOREINFO_NUMBER(VA_BITS);
@@ -15,5 +22,7 @@ void arch_crash_save_vmcoreinfo(void)
 						kimage_voffset);
 	vmcoreinfo_append_str("NUMBER(PHYS_OFFSET)=0x%llx\n",
 						PHYS_OFFSET);
+	vmcoreinfo_append_str("NUMBER(tcr_el1_t1sz)=0x%llx\n",
+						get_tcr_el1_t1sz());
 	vmcoreinfo_append_str("KERNELOFFSET=%lx\n", kaslr_offset());
 }
