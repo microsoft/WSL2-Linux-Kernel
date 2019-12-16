9334316fff76d2715cdf189001b6bf6e5f0506b0
AKASHI Takahiro <takahiro.akashi@linaro.org>
2019-12-16 11:12:47 +0900

    arm64: kexec_file: add crash dump support
    Enabling crash dump (kdump) includes
    * prepare contents of ELF header of a core dump file, /proc/vmcore,
      using crash_prepare_elf64_headers(), and
    * add two device tree properties, "linux,usable-memory-range" and
      "linux,elfcorehdr", which represent respectively a memory range
      to be used by crash dump kernel and the header's location
    
    Signed-off-by: AKASHI Takahiro <takahiro.akashi@linaro.org>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Reviewed-by: James Morse <james.morse@arm.com>
    Tested-and-reviewed-by: Bhupesh Sharma <bhsharma@redhat.com>
    Signed-off-by: Will Deacon <will@kernel.org>
    (cherry picked from commit 3751e728cef2908c15974a5ae44627fd41ef3362)
    Signed-off-by: Pavel Tatashin <pasha.tatashin@soleen.com>

--- arch/arm64/include/asm/kexec.h
+++ arch/arm64/include/asm/kexec.h
@@ -100,6 +100,10 @@ struct kimage_arch {
 	phys_addr_t ima_buffer_addr;
 	size_t ima_buffer_size;
 #endif
+	/* Core ELF header buffer */
+	void *elf_headers;
+	unsigned long elf_headers_mem;
+	unsigned long elf_headers_sz;
 };
 
 #ifdef CONFIG_KEXEC_FILE
