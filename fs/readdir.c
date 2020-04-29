bbf79f398c0a5427e9ec0bf651bd24a334eeb092
Ryan Underwood <runderwood@linkedin.com>
2020-04-29 22:25:12 +0000

    Revert "ext4: allow readdir()'s of large empty directories to be interrupted"
    This reverts commit 1f60fbe7274918adb8db2f616e321890730ab7e3.
    
    Conflicts:
    	fs/compat.c
    	fs/ext4/dir.c
    
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/readdir.c
+++ fs/readdir.c
@@ -220,8 +220,6 @@ static int filldir(struct dir_context *ctx, const char *name, int namlen,
 	}
 	dirent = buf->previous;
 	if (dirent) {
-		if (signal_pending(current))
-			return -EINTR;
 		if (__put_user(offset, &dirent->d_off))
 			goto efault;
 	}
@@ -304,8 +302,6 @@ static int filldir64(struct dir_context *ctx, const char *name, int namlen,
 		return -EINVAL;
 	dirent = buf->previous;
 	if (dirent) {
-		if (signal_pending(current))
-			return -EINTR;
 		if (__put_user(offset, &dirent->d_off))
 			goto efault;
 	}
