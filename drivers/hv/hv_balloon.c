e0e7ed2671bc8a7cc0cb1c51ac27c6b7fa93f874
Sunil Muthuswamy <sunilmut@microsoft.com>
2020-10-28 07:21:53 -0700

    Hyper-V: support for free page reporting
    Linux has support for free page reporting now (36e66c554b5c) for
    virtualized environment. On Hyper-V when virtually backed VMs are
    configured, Hyper-V will advertise cold memory discard capability,
    when supported. This patch adds the support to hook into the free
    page reporting infrastructure and leverage the Hyper-V cold memory
    discard hint hypercall to report/free these pages back to the host.
    
    Signed-off-by: Sunil Muthuswamy <sunilmut@microsoft.com>
    
    TAG_MSFT: wsl

--- drivers/hv/hv_balloon.c
+++ drivers/hv/hv_balloon.c
@@ -21,7 +21,7 @@
 #include <linux/memory.h>
 #include <linux/notifier.h>
 #include <linux/percpu_counter.h>
-
+#include <linux/page_reporting.h>
 #include <linux/hyperv.h>
 #include <asm/hyperv-tlfs.h>
 
@@ -563,6 +563,9 @@ struct hv_dynmem_device {
 	 * The negotiated version agreed by host.
 	 */
 	__u32 version;
+#ifdef CONFIG_PAGE_REPORTING
+	struct page_reporting_dev_info ph_dev_info;
+#endif
 };
 
 static struct hv_dynmem_device dm_device;
@@ -1565,6 +1568,88 @@ static void balloon_onchannelcallback(void *context)
 
 }
 
+#ifdef CONFIG_PAGE_REPORTING
+static int hyperv_discard_pages(struct scatterlist **sgs, int nents)
+{
+	unsigned long flags;
+	struct hv_memory_hint *hint;
+	int i;
+	struct scatterlist *sg;
+	u64 status;
+
+	WARN_ON(nents > HV_MAX_GPA_PAGE_RANGES);
+	local_irq_save(flags);
+	hint = *(struct hv_memory_hint **)this_cpu_ptr(hyperv_pcpu_input_arg);
+	if (!hint) {
+		local_irq_restore(flags);
+		return -EINVAL;
+	}
+
+	hint->type = HV_MEMORY_HINT_TYPE_COLD_DISCARD;
+	hint->reserved = 0;
+	for (i = 0, sg = sgs[0]; sg; sg = sg_next(sg), i++) {
+		int order;
+		union hv_gpa_page_range *range;
+
+		order = get_order(sg->length);
+		range = &hint->ranges[i];
+		range->address_space = 0;
+		range->page.largepage = 1;
+		range->page.additional_pages = (1ull << (order - 9)) - 1;
+		range->base_large_pfn = page_to_pfn(sg_page(sg)) >> 9;
+	}
+
+	WARN_ON(i != nents);
+
+	status = hv_do_rep_hypercall(HVCALL_MEMORY_HEAT_HINT, nents, 0,
+				     hint, NULL);
+	local_irq_restore(flags);
+	status &= HV_HYPERCALL_RESULT_MASK;
+	if (status != HV_STATUS_SUCCESS) {
+		WARN_ONCE(1,
+		"Cold memory discard hypercall failed with status 0x%llx\n",
+			status);
+		return -EINVAL;
+	} else {
+		return 0;
+	}
+}
+
+static int hv_page_hinting(struct page_reporting_dev_info *ph_dev_info,
+		struct scatterlist *sg, unsigned int nents)
+{
+	return hyperv_discard_pages(&sg, nents);
+}
+
+static void enable_page_reporting(void)
+{
+	int ret;
+
+	if (!hyperv_query_ext_cap(HV_CAPABILITY_MEMORY_COLD_DISCARD_HINT)) {
+		pr_info("Cold memory discard hint not supported.\n");
+		return;
+	}
+
+	BUILD_BUG_ON(PAGE_REPORTING_CAPACITY > HV_MAX_GPA_PAGE_RANGES);
+	dm_device.ph_dev_info.report = hv_page_hinting;
+	ret = page_reporting_register(&dm_device.ph_dev_info);
+	if (ret < 0) {
+		pr_err("Failed to enable cold memory discard: %d\n", ret);
+		dm_device.ph_dev_info.report = NULL;
+	} else {
+		pr_info("Cold memory discard hint enabled\n");
+	}
+}
+
+static void disable_hinting(void)
+{
+	if (dm_device.ph_dev_info.report) {
+		page_reporting_unregister(&dm_device.ph_dev_info);
+		dm_device.ph_dev_info.report = NULL;
+	}
+}
+#endif //CONFIG_PAGE_REPORTING
+
 static int balloon_connect_vsp(struct hv_device *dev)
 {
 	struct dm_version_request version_req;
@@ -1710,6 +1795,10 @@ static int balloon_probe(struct hv_device *dev,
 	if (ret != 0)
 		return ret;
 
+#ifdef CONFIG_PAGE_REPORTING
+	enable_page_reporting();
+#endif
+
 	dm_device.state = DM_INITIALIZED;
 
 	dm_device.thread =
@@ -1725,6 +1814,9 @@ probe_error:
 	dm_device.state = DM_INIT_ERROR;
 	dm_device.thread  = NULL;
 	vmbus_close(dev->channel);
+#ifdef CONFIG_PAGE_REPORTING
+	disable_hinting();
+#endif
 #ifdef CONFIG_MEMORY_HOTPLUG
 	unregister_memory_notifier(&hv_memory_nb);
 	restore_online_page_callback(&hv_online_page);
@@ -1747,6 +1839,9 @@ static int balloon_remove(struct hv_device *dev)
 
 	kthread_stop(dm->thread);
 	vmbus_close(dev->channel);
+#ifdef CONFIG_PAGE_REPORTING
+	disable_hinting();
+#endif
 #ifdef CONFIG_MEMORY_HOTPLUG
 	unregister_memory_notifier(&hv_memory_nb);
 	restore_online_page_callback(&hv_online_page);
