658eeb55b145e20b08167a6a154bc353f0efc5d6
Taylor Stark <tstark@linux.microsoft.com>
2021-07-15 15:36:38 -0700

    virtio-pmem: Set DRIVER_OK status prior to creating pmem region
    Update virtio-pmem to call virtio_device_ready prior to creating the pmem
    region. Otherwise, the guest may try to access the pmem region prior to
    the DRIVER_OK status being set.
    
    In the case of Hyper-V, the backing pmem file isn't mapped to the guest
    until the DRIVER_OK status is set. Therefore, attempts to access the pmem
    region can cause the guest to crash. Hyper-V could map the file earlier,
    for example at VM creation, but we didn't want to pay the mapping cost if
    the device is never used. Additionally, it felt weird to allow the guest
    to access the region prior to the device fully coming online.
    
    Signed-off-by: Taylor Stark <tstark@microsoft.com>
    Reviewed-by: Pankaj Gupta <pankaj.gupta@ionos.com>
    
    Link: https://lore.kernel.org/r/20210715223638.GA29649@linuxonhyperv3.guj3yctzbm1etfxqx2vob5hsef.xx.internal.cloudapp.net
    Signed-off-by: Tyler Hicks <tyhicks@linux.microsoft.com>

--- drivers/nvdimm/virtio_pmem.c
+++ drivers/nvdimm/virtio_pmem.c
@@ -91,6 +91,11 @@ static int virtio_pmem_probe(struct virtio_device *vdev)
 
 	dev_set_drvdata(&vdev->dev, vpmem->nvdimm_bus);
 
+	/* Online the device prior to creating a pmem region, to ensure that
+	 * the region is never touched while the device is offline.
+	 */
+	virtio_device_ready(vdev);
+
 	ndr_desc.res = &res;
 	ndr_desc.numa_node = nid;
 	ndr_desc.flush = async_pmem_flush;
@@ -111,6 +116,7 @@ static int virtio_pmem_probe(struct virtio_device *vdev)
 	}
 	return 0;
 out_nd:
+	vdev->config->reset(vdev);
 	virtio_reset_device(vdev);
 	nvdimm_bus_unregister(vpmem->nvdimm_bus);
 out_vq:
