11e60be4086e808b399192caef31ddcf33bc952b
Eric Biggers <ebiggers@google.com>
2021-07-16 09:39:15 -0500

    f2fs: fix the f2fs_file_write_iter tracepoint
    [ Upstream commit ccf7cf92373d1a53166582013430b3b9c05a6ba2 ]
    
    Pass in the original position and count rather than the position and
    count that were updated by the write.  Also use the correct types for
    all arguments, in particular the file offset which was being truncated
    to 32 bits on 32-bit platforms.
    
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Stable-dep-of: bb34cc6ca87f ("f2fs: fix to update iostat correctly in f2fs_filemap_fault()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -4255,6 +4255,8 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
 	struct file *file = iocb->ki_filp;
 	struct inode *inode = file_inode(file);
+	const loff_t orig_pos = iocb->ki_pos;
+	const size_t orig_count = iov_iter_count(from);
 	ssize_t ret;
 
 	if (unlikely(f2fs_cp_error(F2FS_I_SB(inode)))) {
@@ -4358,8 +4360,7 @@ write:
 unlock:
 	inode_unlock(inode);
 out:
-	trace_f2fs_file_write_iter(inode, iocb->ki_pos,
-					iov_iter_count(from), ret);
+	trace_f2fs_file_write_iter(inode, orig_pos, orig_count, ret);
 	if (ret > 0)
 		ret = generic_write_sync(iocb, ret);
 	return ret;
