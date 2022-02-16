8a3d371e9f3e2691bce546e21fd90416e3b4ff04
Iouri Tarassov <iourit@linux.microsoft.com>
2022-02-15 18:11:52 -0800

    drivers: hv: dxgkrnl: Add virtual compute device VMBus channel guids
    Add VMBus channel guids, which are used by hyper-v virtual compute
    device driver.
    
    Signed-off-by: Iouri Tarassov <iourit@linux.microsoft.com>
    [kms: forward port to 6.6 from 6.1. No code changes made.]
    Signed-off-by: Kelsey Steele <kelseysteele@microsoft.com>

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -1452,6 +1452,22 @@ void vmbus_free_mmio(resource_size_t start, resource_size_t size);
 			  0x05, 0x58, 0xeb, 0x10, 0x73, 0xf8)
 
 /*
+ * GPU paravirtualization global DXGK channel
+ * {DDE9CBC0-5060-4436-9448-EA1254A5D177}
+ */
+#define HV_GPUP_DXGK_GLOBAL_GUID \
+	.guid = GUID_INIT(0xdde9cbc0, 0x5060, 0x4436, 0x94, 0x48, \
+			  0xea, 0x12, 0x54, 0xa5, 0xd1, 0x77)
+
+/*
+ * GPU paravirtualization per virtual GPU DXGK channel
+ * {6E382D18-3336-4F4B-ACC4-2B7703D4DF4A}
+ */
+#define HV_GPUP_DXGK_VGPU_GUID \
+	.guid = GUID_INIT(0x6e382d18, 0x3336, 0x4f4b, 0xac, 0xc4, \
+			  0x2b, 0x77, 0x3, 0xd4, 0xdf, 0x4a)
+
+/*
  * Synthetic FC GUID
  * {2f9bcc4a-0069-4af3-b76b-6fd0be528cda}
  */
