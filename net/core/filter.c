07aa35a50fe68845a37ff9ae663ac509fe9d3205
Martin KaFai Lau <martin.lau@kernel.org>
2022-09-01 17:28:02 -0700

    bpf: net: Change sk_getsockopt() to take the sockptr_t argument
    [ Upstream commit 4ff09db1b79b98b4a2a7511571c640b76cab3beb ]
    
    This patch changes sk_getsockopt() to take the sockptr_t argument
    such that it can be used by bpf_getsockopt(SOL_SOCKET) in a
    latter patch.
    
    security_socket_getpeersec_stream() is not changed.  It stays
    with the __user ptr (optval.user and optlen.user) to avoid changes
    to other security hooks.  bpf_getsockopt(SOL_SOCKET) also does not
    support SO_PEERSEC.
    
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Link: https://lore.kernel.org/r/20220902002802.2888419-1-kafai@fb.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Stable-dep-of: 5c3be3e0eb44 ("ipmr: fix incorrect parameter validation in the ip_mroute_getsockopt() function")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- net/core/filter.c
+++ net/core/filter.c
@@ -10316,8 +10316,7 @@ int sk_detach_filter(struct sock *sk)
 }
 EXPORT_SYMBOL_GPL(sk_detach_filter);
 
-int sk_get_filter(struct sock *sk, struct sock_filter __user *ubuf,
-		  unsigned int len)
+int sk_get_filter(struct sock *sk, sockptr_t optval, unsigned int len)
 {
 	struct sock_fprog_kern *fprog;
 	struct sk_filter *filter;
@@ -10348,7 +10347,7 @@ int sk_get_filter(struct sock *sk, struct sock_filter __user *ubuf,
 		goto out;
 
 	ret = -EFAULT;
-	if (copy_to_user(ubuf, fprog->filter, bpf_classic_proglen(fprog)))
+	if (copy_to_sockptr(optval, fprog->filter, bpf_classic_proglen(fprog)))
 		goto out;
 
 	/* Instead of bytes, the API requests to return the number
