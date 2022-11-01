fba4c748cc60744cf0a1235a23101a29562c42f4
Dexuan Cui <decui@microsoft.com>
2022-10-31 19:17:05 -0700

    vsock: remove the unused 'wait' in vsock_connectible_recvmsg()
    Remove the unused variable introduced by 19c1b90e1979.
    
    Fixes: 19c1b90e1979 ("af_vsock: separate receive data loop")
    Signed-off-by: Dexuan Cui <decui@microsoft.com>
    Reviewed-by: Stefano Garzarella <sgarzare@redhat.com>
    Signed-off-by: Kelsey Steele <kelseysteele@microsoft.com>

--- net/vmw_vsock/af_vsock.c
+++ net/vmw_vsock/af_vsock.c
@@ -2068,8 +2068,6 @@ vsock_connectible_recvmsg(struct socket *sock, struct msghdr *msg, size_t len,
 	const struct vsock_transport *transport;
 	int err;
 
-	DEFINE_WAIT(wait);
-
 	sk = sock->sk;
 	vsk = vsock_sk(sk);
 	err = 0;
