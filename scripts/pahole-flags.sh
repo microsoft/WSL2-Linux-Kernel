66cb0868b1238f35651613e8b113527a2bdb97d1
Alan Maguire <alan.maguire@oracle.com>
2023-05-10 14:02:41 +0100

    bpf: Add --skip_encoding_btf_inconsistent_proto, --btf_gen_optimized to pahole flags for v1.25
    commit 7b99f75942da332e3f4f865e55a10fec95a30d4f upstream.
    
    v1.25 of pahole supports filtering out functions with multiple inconsistent
    function prototypes or optimized-out parameters from the BTF representation.
    These present problems because there is no additional info in BTF saying which
    inconsistent prototype matches which function instance to help guide attachment,
    and functions with optimized-out parameters can lead to incorrect assumptions
    about register contents.
    
    So for now, filter out such functions while adding BTF representations for
    functions that have "."-suffixes (foo.isra.0) but not optimized-out parameters.
    This patch assumes that below linked changes land in pahole for v1.25.
    
    Issues with pahole filtering being too aggressive in removing functions
    appear to be resolved now, but CI and further testing will confirm.
    
    Signed-off-by: Alan Maguire <alan.maguire@oracle.com>
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20230510130241.1696561-1-alan.maguire@oracle.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    [ small context conflict because of not backported --lang_exclude=rust
    option, which is not needed in 5.15 ]
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- scripts/pahole-flags.sh
+++ scripts/pahole-flags.sh
@@ -20,5 +20,8 @@ fi
 if [ "${pahole_ver}" -ge "124" ]; then
 	extra_paholeopt="${extra_paholeopt} --skip_encoding_btf_enum64"
 fi
+if [ "${pahole_ver}" -ge "125" ]; then
+	extra_paholeopt="${extra_paholeopt} --skip_encoding_btf_inconsistent_proto --btf_gen_optimized"
+fi
 
 echo ${extra_paholeopt}
