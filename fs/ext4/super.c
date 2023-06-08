86ebb5b5344d5ee3eecd0dff7376d759a7a9a050
Theodore Ts'o <tytso@mit.edu>
2023-06-08 09:57:04 -0400

    Revert "ext4: don't clear SB_RDONLY when remounting r/w until quota is re-enabled"
    commit 1b29243933098cdbc31b579b5616e183b4275e2f upstream.
    
    This reverts commit a44be64bbecb15a452496f60db6eacfee2b59c79.
    
    Link: https://lore.kernel.org/r/653b3359-2005-21b1-039d-c55ca4cffdcc@gmail.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -5748,7 +5748,6 @@ static int ext4_remount(struct super_block *sb, int *flags, char *data)
 	struct ext4_mount_options old_opts;
 	ext4_group_t g;
 	int err = 0;
-	int enable_rw = 0;
 #ifdef CONFIG_QUOTA
 	int enable_quota = 0;
 	int i, j;
@@ -5949,7 +5948,7 @@ static int ext4_remount(struct super_block *sb, int *flags, char *data)
 			if (err)
 				goto restore_opts;
 
-			enable_rw = 1;
+			sb->s_flags &= ~SB_RDONLY;
 			if (ext4_has_feature_mmp(sb)) {
 				err = ext4_multi_mount_protect(sb,
 						le64_to_cpu(es->s_mmp_block));
@@ -5995,9 +5994,6 @@ static int ext4_remount(struct super_block *sb, int *flags, char *data)
 #endif
 	if (!test_opt(sb, BLOCK_VALIDITY) && sbi->s_system_blks)
 		ext4_release_system_zone(sb);
-
-	if (enable_rw)
-		sb->s_flags &= ~SB_RDONLY;
 
 	/*
 	 * Reinitialize lazy itable initialization thread based on
