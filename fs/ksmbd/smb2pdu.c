62e6846ee3bae7fc2607f30136b5566a7c4685bf
Atte Heikkilä <atteh.mailbox@gmail.com>
2023-12-19 00:33:12 +0900

    ksmbd: casefold utf-8 share names and fix ascii lowercase conversion
    [ Upstream commit 16b5f54e30c1ddec36bdf946a299b3254aace477 ]
    
    strtolower() corrupts all UTF-8 share names that have a byte in the C0
    (À ISO8859-1) to DE (Þ ISO8859-1) range, since the non-ASCII part of
    ISO8859-1 is incompatible with UTF-8. Prevent this by checking that a
    byte is in the ASCII range with isascii(), before the conversion to
    lowercase with tolower(). Properly handle case-insensitivity of UTF-8
    share names by casefolding them, but fallback to ASCII lowercase
    conversion on failure or if CONFIG_UNICODE is not set. Refactor to move
    the share name casefolding immediately after the share name extraction.
    Also, make the associated constness corrections.
    
    Signed-off-by: Atte Heikkilä <atteh.mailbox@gmail.com>
    Acked-by: Namjae Jeon <linkinjeon@kernel.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/ksmbd/smb2pdu.c
+++ fs/ksmbd/smb2pdu.c
@@ -1924,7 +1924,7 @@ int smb2_tree_connect(struct ksmbd_work *work)
 		goto out_err1;
 	}
 
-	name = ksmbd_extract_sharename(treename);
+	name = ksmbd_extract_sharename(conn->um, treename);
 	if (IS_ERR(name)) {
 		status.ret = KSMBD_TREE_CONN_STATUS_ERROR;
 		goto out_err1;
