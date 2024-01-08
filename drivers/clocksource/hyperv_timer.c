b63263237d73f1e1d76e2c2049e31b607ef78bec
Allen Pais <apais@linux.microsoft.com>
2024-01-08 17:21:59 +0000

    arm64: hyperv: Fix build breakage for non-ARM64 architectures
    Commit [arm64: hyperv: Enable Hyper-V synthetic clocks/timers]
    introduced changes to enable Hyper-V synthetic clocks and timers
    on ARM64. However, these changes led to build breakage on
    non-ARM64 architectures due to the use of the '__bss_decrypted'
    section, which is notavailable on those platforms.
    
    To address this issue, this commit adds conditional compilation to
    use'__bss_decrypted' only on ARM64 architectures. The introduced
    '#ifdefCONFIG_ARM64' ensures that the code involving
    '__bss_decrypted' is included only when building for ARM64, preventing
    build failures on other platforms.
    
    This modification allows the Hyper-V support code to be selectively
    applied, ensuring compatibility with both ARM64 and non-ARM64
    architectures.
    
    Signed-off-by: Allen Pais <apais@microsoft.com>

--- drivers/clocksource/hyperv_timer.c
+++ drivers/clocksource/hyperv_timer.c
@@ -376,7 +376,11 @@ static __always_inline u64 read_hv_clock_msr(void)
 	 * noinstr. Notable; while HV_REGISTER_TIME_REF_COUNT is a synthetic
 	 * register it doesn't need the GHCB path.
 	 */
+#ifdef CONFIG_ARM64
+	return hv_get_register(HV_REGISTER_TIME_REF_COUNT);
+#else
 	return hv_raw_get_register(HV_REGISTER_TIME_REF_COUNT);
+#endif
 }
 
 /*
@@ -390,7 +394,12 @@ static __always_inline u64 read_hv_clock_msr(void)
 static union {
 	struct ms_hyperv_tsc_page page;
 	u8 reserved[PAGE_SIZE];
-} tsc_pg __bss_decrypted __aligned(PAGE_SIZE);
+} tsc_pg
+#ifdef CONFIG_ARM64
+    __aligned(PAGE_SIZE);
+#else
+    __bss_decrypted __aligned(PAGE_SIZE);
+#endif
 
 static struct ms_hyperv_tsc_page *tsc_page = &tsc_pg.page;
 static unsigned long tsc_pfn;
