da1438d6cb86a83e75801cc8582b636ccc32cf43
Iouri Tarassov <iourit@linux.microsoft.com>
2024-06-26 14:56:00 -0700

    drivers: hv: dxgkrnl: Make dxgkrnl to compile with new VMBUS interface"
    Signed-off-by: Iouri Tarassov <iourit@linux.microsoft.com>

--- drivers/hv/dxgkrnl/dxgkrnl.h
+++ drivers/hv/dxgkrnl/dxgkrnl.h
@@ -43,6 +43,8 @@ struct dxgsyncobject;
 struct dxgsharedsyncobject;
 struct dxghwqueue;
 
+#define _MAIN_KERNEL_ 1
+
 /*
  * Driver private data.
  * A single /dev/dxg device is created per virtual machine.
