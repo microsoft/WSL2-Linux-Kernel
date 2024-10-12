dd5d32f74fc1a48302a40cf3e259dd3d4b8e78f0
Linus Walleij <linus.walleij@linaro.org>
2024-10-12 22:35:23 +0200

    net: phy: mdio-bcm-unimac: Add BCM6846 support
    [ Upstream commit 906b77ca91c7e9833b4e47bedb6bec76be71d497 ]
    
    Add Unimac mdio compatible string for the special BCM6846
    variant.
    
    This variant has a few extra registers compared to other
    versions.
    
    Suggested-by: Florian Fainelli <florian.fainelli@broadcom.com>
    Link: https://lore.kernel.org/linux-devicetree/b542b2e8-115c-4234-a464-e73aa6bece5c@broadcom.com/
    Signed-off-by: Linus Walleij <linus.walleij@linaro.org>
    Link: https://patch.msgid.link/20241012-bcm6846-mdio-v1-2-c703ca83e962@linaro.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/mdio/mdio-bcm-unimac.c
+++ drivers/net/mdio/mdio-bcm-unimac.c
@@ -336,6 +336,7 @@ static SIMPLE_DEV_PM_OPS(unimac_mdio_pm_ops,
 static const struct of_device_id unimac_mdio_ids[] = {
 	{ .compatible = "brcm,asp-v2.1-mdio", },
 	{ .compatible = "brcm,asp-v2.0-mdio", },
+	{ .compatible = "brcm,bcm6846-mdio", },
 	{ .compatible = "brcm,genet-mdio-v5", },
 	{ .compatible = "brcm,genet-mdio-v4", },
 	{ .compatible = "brcm,genet-mdio-v3", },
