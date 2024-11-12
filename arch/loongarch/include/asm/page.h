7ed79c551c09067005ac6fcd183d0edec8804ad3
Yuli Wang <wangyuli@uniontech.com>
2024-11-12 16:35:39 +0800

    LoongArch: Define a default value for VM_DATA_DEFAULT_FLAGS
    [ Upstream commit c859900a841b0a6cd9a73d16426465e44cdde29c ]
    
    This is a trivial cleanup, commit c62da0c35d58518d ("mm/vma: define a
    default value for VM_DATA_DEFAULT_FLAGS") has unified default values of
    VM_DATA_DEFAULT_FLAGS across different platforms.
    
    Apply the same consistency to LoongArch.
    
    Suggested-by: Wentao Guan <guanwentao@uniontech.com>
    Signed-off-by: Yuli Wang <wangyuli@uniontech.com>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/loongarch/include/asm/page.h
+++ arch/loongarch/include/asm/page.h
@@ -94,10 +94,7 @@ typedef struct { unsigned long pgprot; } pgprot_t;
 extern int __virt_addr_valid(volatile void *kaddr);
 #define virt_addr_valid(kaddr)	__virt_addr_valid((volatile void *)(kaddr))
 
-#define VM_DATA_DEFAULT_FLAGS \
-	(VM_READ | VM_WRITE | \
-	 ((current->personality & READ_IMPLIES_EXEC) ? VM_EXEC : 0) | \
-	 VM_MAYREAD | VM_MAYWRITE | VM_MAYEXEC)
+#define VM_DATA_DEFAULT_FLAGS	VM_DATA_FLAGS_TSK_EXEC
 
 #include <asm-generic/memory_model.h>
 #include <asm-generic/getorder.h>
