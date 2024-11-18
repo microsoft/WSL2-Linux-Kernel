73934e535cffbda1490fa97d82690a0f9aa73e94
Paul Aurich <paul@darkrain42.org>
2024-11-18 13:50:28 -0800

    smb: During unmount, ensure all cached dir instances drop their dentry
    commit 3fa640d035e5ae526769615c35cb9ed4be6e3662 upstream.
    
    The unmount process (cifs_kill_sb() calling close_all_cached_dirs()) can
    race with various cached directory operations, which ultimately results
    in dentries not being dropped and these kernel BUGs:
    
    BUG: Dentry ffff88814f37e358{i=1000000000080,n=/}  still in use (2) [unmount of cifs cifs]
    VFS: Busy inodes after unmount of cifs (cifs)
    ------------[ cut here ]------------
    kernel BUG at fs/super.c:661!
    
    This happens when a cfid is in the process of being cleaned up when, and
    has been removed from the cfids->entries list, including:
    
    - Receiving a lease break from the server
    - Server reconnection triggers invalidate_all_cached_dirs(), which
      removes all the cfids from the list
    - The laundromat thread decides to expire an old cfid.
    
    To solve these problems, dropping the dentry is done in queued work done
    in a newly-added cfid_put_wq workqueue, and close_all_cached_dirs()
    flushes that workqueue after it drops all the dentries of which it's
    aware. This is a global workqueue (rather than scoped to a mount), but
    the queued work is minimal.
    
    The final cleanup work for cleaning up a cfid is performed via work
    queued in the serverclose_wq workqueue; this is done separate from
    dropping the dentries so that close_all_cached_dirs() doesn't block on
    any server operations.
    
    Both of these queued works expect to invoked with a cfid reference and
    a tcon reference to avoid those objects from being freed while the work
    is ongoing.
    
    While we're here, add proper locking to close_all_cached_dirs(), and
    locking around the freeing of cfid->dentry.
    
    Fixes: ebe98f1447bb ("cifs: enable caching of directories for which a lease is held")
    Cc: stable@vger.kernel.org
    Signed-off-by: Paul Aurich <paul@darkrain42.org>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/smb/client/cifsfs.c
+++ fs/smb/client/cifsfs.c
@@ -156,6 +156,7 @@ struct workqueue_struct	*fileinfo_put_wq;
 struct workqueue_struct	*cifsoplockd_wq;
 struct workqueue_struct	*deferredclose_wq;
 struct workqueue_struct	*serverclose_wq;
+struct workqueue_struct	*cfid_put_wq;
 __u32 cifs_lock_secret;
 
 /*
@@ -1899,9 +1900,16 @@ init_cifs(void)
 		goto out_destroy_deferredclose_wq;
 	}
 
+	cfid_put_wq = alloc_workqueue("cfid_put_wq",
+				      WQ_FREEZABLE|WQ_MEM_RECLAIM, 0);
+	if (!cfid_put_wq) {
+		rc = -ENOMEM;
+		goto out_destroy_serverclose_wq;
+	}
+
 	rc = cifs_init_inodecache();
 	if (rc)
-		goto out_destroy_serverclose_wq;
+		goto out_destroy_cfid_put_wq;
 
 	rc = init_mids();
 	if (rc)
@@ -1963,6 +1971,8 @@ out_destroy_mids:
 	destroy_mids();
 out_destroy_inodecache:
 	cifs_destroy_inodecache();
+out_destroy_cfid_put_wq:
+	destroy_workqueue(cfid_put_wq);
 out_destroy_serverclose_wq:
 	destroy_workqueue(serverclose_wq);
 out_destroy_deferredclose_wq:
