c7e65cab54a89f4df54110f0b44c4ade93d1a911
Wayne Lin <wayne.lin@amd.com>
2024-11-26 09:34:03 +0800

    drm/amd/display: Don't refer to dc_sink in is_dsc_need_re_compute
    [ Upstream commit fcf6a49d79923a234844b8efe830a61f3f0584e4 ]
    
    [Why]
    When unplug one of monitors connected after mst hub, encounter null pointer dereference.
    
    It's due to dc_sink get released immediately in early_unregister() or detect_ctx(). When
    commit new state which directly referring to info stored in dc_sink will cause null pointer
    dereference.
    
    [how]
    Remove redundant checking condition. Relevant condition should already be covered by checking
    if dsc_aux is null or not. Also reset dsc_aux to NULL when the connector is disconnected.
    
    Reviewed-by: Jerry Zuo <jerry.zuo@amd.com>
    Acked-by: Zaeem Mohamed <zaeem.mohamed@amd.com>
    Signed-off-by: Wayne Lin <wayne.lin@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    [ Resolve minor conflicts ]
    Signed-off-by: Bin Lan <bin.lan.cn@windriver.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm_mst_types.c
@@ -179,6 +179,8 @@ amdgpu_dm_mst_connector_early_unregister(struct drm_connector *connector)
 		dc_sink_release(dc_sink);
 		aconnector->dc_sink = NULL;
 		aconnector->edid = NULL;
+		aconnector->dsc_aux = NULL;
+		port->passthrough_aux = NULL;
 	}
 
 	aconnector->mst_status = MST_STATUS_DEFAULT;
@@ -487,6 +489,8 @@ dm_dp_mst_detect(struct drm_connector *connector,
 		dc_sink_release(aconnector->dc_sink);
 		aconnector->dc_sink = NULL;
 		aconnector->edid = NULL;
+		aconnector->dsc_aux = NULL;
+		port->passthrough_aux = NULL;
 
 		amdgpu_dm_set_mst_status(&aconnector->mst_status,
 			MST_REMOTE_EDID | MST_ALLOCATE_NEW_PAYLOAD | MST_CLEAR_ALLOCATED_PAYLOAD,
